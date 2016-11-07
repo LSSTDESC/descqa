248fbd08f2ed2bc9735872e2c9a9318998511e19
Tom Uram <tomuram@gmail.com>
2016-11-06 21:40:56 -0800

    Centralize catalog definitions

--- catalog/descqaGlobalConfig.py
+++ catalog/descqaGlobalConfig.py
@@ -31,32 +31,39 @@ class SimCatalog:
 
 
 # observation catalogs
-LIWHITE_STELLAR_MASS='Li White'
-MB2_STELLAR_MASS='Massive Black II'
+LIWHITE_STELLAR_MASS=0
+MB2_STELLAR_MASS=1
+MB2_STELLAR_MASS_HALO_MASS=2
+SDSS_COLOR=3
 
 def get_obs_catalog(name):
-	if(name is LIWHITE_STELLAR_MASS)				: return ObsCatalog(name,'LIWHITE/StellarMassFunction/massfunc_data.txt',(0,4))
-	if(name is MB2_STELLAR_MASS)						: return ObsCatalog(name,'MASSIVEBLACKII/StellarMassFunction/massfunc_data.txt',(0,1))
-	if(name is MB2_STELLAR_MASS_HALO_MASS)	: return ObsCatalog(name,'MASSIVEBLACKII/StellarMassHaloMass/tab.txt',(0,2,3,4))
-	raise 'Unknown catalog:',name
+	if(name == LIWHITE_STELLAR_MASS)		: return ObsCatalog('Li White','LIWHITE/StellarMassFunction/massfunc_data.txt',(0,4))
+	if(name == MB2_STELLAR_MASS)			: return ObsCatalog('Massive Black II','MASSIVEBLACKII/StellarMassFunction/massfunc_data.txt',(0,1))
+	if(name == MB2_STELLAR_MASS_HALO_MASS)	: return ObsCatalog('Massive Black II','MASSIVEBLACKII/StellarMassHaloMass/tab.txt',(0,2,3,4))
+	if(name == SDSS_COLOR)					: return ObsCatalog('SDSS','SDSS/',(0,1))
+	raise Exception('Unknown obs catalog: %d' % name)
 
 
 # simulation catalogs
-CMU_MB2 = 'CMU MB2'
-GALACTICUS_MB2 = 'Galacticus MB2'
-SAG = 'SAG'
-SHAM_MB2 = 'SHAM MB2'
-SHAM_LIWHITE = 'SHAM Li White'
-YALE_MB2 = 'Yale MB2'
-YALE_LIWHITE = 'Yale Li White'
-IHOD = 'iHOD'
+CMU_MB2 = 0
+GALACTICUS_MB2 = 1
+SAG = 2
+SHAM_MB2 = 3
+SHAM_LIWHITE = 4
+YALE_MB2 = 5
+YALE_LIWHITE = 6
+IHOD = 7
 
 def get_sim_catalog(name):
-	if(name is CMU_MB2)					: return SimCatalog(name,'catalog.hdf5.MB2')
-	if(name is GALACTICUS_MB2)  : return SimCatalog(name,'galacticus_mb2_anl.hdf5.galacticus')
-	if(name is SAG_MB2)					: return SimCatalog(name,'SAGcatalog.sag')
-	if(name is SHAM_LIWHITE)	  : return SimCatalog(name,'SHAM_0.94118.npy')
-	raise 'Unknown catalog:',name
+	if(name is CMU_MB2)			: return SimCatalog('CMU MB2','catalog.hdf5.MB2')
+	if(name is GALACTICUS_MB2)  : return SimCatalog('Galacticus MB2','galacticus_mb2_anl.hdf5.galacticus')
+	if(name is SAG)				: return SimCatalog('SAG','SAGcatalog.sag')
+	if(name is SHAM_MB2)	    : return SimCatalog('SHAM MB2','SHAM_0.94118_MBII.npy')
+	if(name is SHAM_LIWHITE)    : return SimCatalog('SHAM Li White','SHAM_0.94118.npy')
+	if(name is YALE_MB2)	  	: return SimCatalog('Yale MB2','yale_cam_age_matching_MBII_rho-1.0_sigma0.2_z0.0.hdf5.yale')
+	if(name is YALE_LIWHITE)    : return SimCatalog('Yale Li White','yale_cam_age_matching_LiWhite_2009_rho-1.0_sigma0.2_z0.0.hdf5.yale')
+	if(name is IHOD)	  		: return SimCatalog('iHOD','iHODcatalog_v0.h5.iHOD')
+	raise Exception('Unknown sim catalog: %d' % name)
 
 """
 With the changes above, the par files will change from the current version as follows:
