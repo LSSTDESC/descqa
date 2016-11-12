a8746e403b635293b663eb1934a3b9a9911b1664
Yao-Yuan Mao <yymao.astro@gmail.com>
2016-11-12 11:45:51 -0800

    document run_master.sh

--- run_master.sh
+++ run_master.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
+# set DESCQA root directory
+DESCQAROOTDIR="/project/projectdirs/lsst/descqa"
+
+# load python module and set PYTHONPATH
+export PYTHONPATH=""
 module load python/2.7.9
 module load numpy/1.9.2
 module load scipy/0.15.1
 module load matplotlib/1.4.3
 module load h5py/2.5.0
 module load cython/0.22
-
-DESCQAROOTDIR="/project/projectdirs/lsst/descqa"
-
 export PYTHONPATH="$DESCQAROOTDIR/lib/python2.7/site-packages:$PYTHONPATH"
+
+# set other necessary paths
 OUTPUTDIR="$DESCQAROOTDIR/run/edison"
 CATALOGDIR="$DESCQAROOTDIR/catalog"
 
-umask 0002 # to ensure permission of output is correct
+# ensure permission of output is readable by the web interface
+umask 0002
+
+# run master.py
 python master.py $OUTPUTDIR --catalog-dir $CATALOGDIR $@
+
