8a36ce4831ecc1d71d5971f4ffeb18178dc774fe
Yao-Yuan Mao <yymao.astro@gmail.com>
2016-11-13 09:57:05 -0800

    fix typo in viewFile.py

--- flashTestView/viewer/viewFile.py
+++ flashTestView/viewer/viewFile.py
@@ -5,13 +5,15 @@ cgitb.enable()
 form = cgi.FieldStorage()
 targetFile  = form.getfirst('target_file')
 
-if targetFile.lower().endswith('.png'):
+if not targetFile:
+    print 'Content-type: text/html\n'
+elif targetFile.lower().endswith('.png'):
     print 'Content-type: text/html\n'
     print '<!DOCTYPE html>'
     print '<html><body>'
     print '<img src="data:image/png;base64,{}" width="100%">'.format(open(targetFile, 'rb').read().encode('base64').replace('\n', ''))
     print '</body></html>'
 else:
-    print 'Content-type: text/text\n'
+    print 'Content-type: text/plain\n'
     print open(targetFile).read()
 
