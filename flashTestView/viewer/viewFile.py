f4980e1aa4667172abcd998c6250d95b27e00898
Yao-Yuan Mao <yymao.astro@gmail.com>
2016-11-13 10:06:40 -0800

    build error-safe in viewFile.py

--- flashTestView/viewer/viewFile.py
+++ flashTestView/viewer/viewFile.py
@@ -5,15 +5,21 @@ cgitb.enable()
 form = cgi.FieldStorage()
 targetFile  = form.getfirst('target_file')
 
-if not targetFile:
-    print 'Content-type: text/html\n'
-elif targetFile.lower().endswith('.png'):
-    print 'Content-type: text/html\n'
-    print '<!DOCTYPE html>'
-    print '<html><body>'
-    print '<img src="data:image/png;base64,{}" width="100%">'.format(open(targetFile, 'rb').read().encode('base64').replace('\n', ''))
-    print '</body></html>'
-else:
+try:
+    with open(targetFile, 'rb') as f:
+        file_content = f.read()
+except (OSError, IOError):
     print 'Content-type: text/plain\n'
-    print open(targetFile).read()
+    print '[Error] Cannot open/read file {}'.format(targetFile)
+    
+else:
+    if targetFile.lower().endswith('.png'):
+        print 'Content-type: text/html\n'
+        print '<!DOCTYPE html>'
+        print '<html><body>'
+        print '<img src="data:image/png;base64,{}" width="100%">'.format(file_content.encode('base64').replace('\n', ''))
+        print '</body></html>'
+    else:
+        print 'Content-type: text/plain\n'
+        print file_content
 
