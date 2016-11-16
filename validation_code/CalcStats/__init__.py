Updated to return test_results dict; fixed L2Diff to return average L2 per point; added write_summary_details module

validation_code/CalcStats/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import division, print_function
 import numpy as np
 
-def L2Diff(dataset_1, dataset_2, threshold=1.0):
+def L2Diff(dataset_1, dataset_2, threshold=1.0, details=False):
     """
     compute the L2 norm for 2 datasets
     
@@ -53,7 +53,16 @@ def L2Diff(dataset_1, dataset_2, threshold=1.0):
         e1 = dataset_1['dy'][mask]
     else:
         e1 = None
-    
+
+    #check for zero errors and remove
+    if e1 is not None:
+        emask = (e1 > 0.)
+        if (np.sum(emask) < len(emask)):
+            print ("Removed "+str(len(emask)-np.sum(emask))+" zero-error points from dataset #1 for L2 test")
+            e1=e1[emask]
+            x1=x1[emask]
+            y1=y1[emask]
+                
     #clean up catalog 2 data to remove nans and infs
     mask=np.isfinite(np.vstack(dataset_2.values())).all(axis=0)
     x2 = dataset_2['x'][mask]
@@ -65,6 +74,15 @@ def L2Diff(dataset_1, dataset_2, threshold=1.0):
         e2 = dataset_2['dy'][mask]
     else:
         e2 = None
+
+    #check for zero errors and remove
+    if e2 is not None:
+        emask = (e2 > 0.)
+        if (np.sum(emask) < len(emask)):
+            print ("Removed "+str(len(emask)-np.sum(emask))+" zero-error points from dataset #2 for L2 test")
+            e2=e2[emask]
+            x2=x2[emask]
+            y2=y2[emask]
     
     #ensure ranges of catalog and validation data are the same
     minx=max(np.min(x1),np.min(x2))
@@ -90,33 +108,49 @@ def L2Diff(dataset_1, dataset_2, threshold=1.0):
         if e1 is not None:
             e1int = e1
     
-    #compute L2 norm and significance
+    #compute L2 norm
     if e1 is not None:
         if e2 is not None:
-            L2 = (np.sum( (y2 - y1int)**2 / (e1int**2 + e2**2) ))
+            diff = (y2 - y1int)**2 / (e1int**2 + e2**2)            
         else:
-            L2 = (np.sum( (y2 - y1int)**2 / e1int**2 ))
+            diff = (y2 - y1int)**2 / e1int**2
     else:
         if e2 is not None:
-            L2 = (np.sum( (y2 - y1int)**2 / e2**2 ))
+            diff = (y2 - y1int)**2 / e2**2 
         else:
-            L2 = (np.sum((y2 - y1int)**2) )
+            diff = (y2 - y1int)**2
+    diff=np.sqrt(diff)
+    L2= np.sum(diff)
     
     #normalize by the number of points
     N_points = 1.0*len(y2)
-    L2 = np.sqrt(L2/N_points)
-    
+    L2 = L2/N_points
+
     #return result
     if (L2 > threshold) or (np.isnan(L2)):
         success = False
     else:
         success = True
     
-    return L2, success
-    
+    if (details):
+        #save detailed results to dictionary if requested
+        if e1 is not None:
+            if e2 is not None:
+                results={'diff':diff,'x':x2,'y1int':y1int,'y2':y2,'e1int':e1int,'e2':e2}
+            else:
+                results={'diff':diff,'x':x2,'y1int':y1int,'y2':y2,'e1int':e1int}
+        else:
+            if e2 is not None:
+                results={'diff':diff,'x':x2,'y1int':y1int,'y2':y2,'e2':e2}
+            else:
+                results={'diff':diff,'x':x2,'y1int':y1int,'y2':y2}
+
+        return L2, success, results
+    else:
+        return L2, success
     
 
-def L1Diff(dataset_1, dataset_2, threshold=1.0):
+def L1Diff(dataset_1, dataset_2, threshold=1.0, details=False):
     """
     compute the L1 norm for 2 datasets
     
@@ -178,7 +212,8 @@ def L1Diff(dataset_1, dataset_2, threshold=1.0):
         y1int=y1
     
     #compute L1 norm and significance
-    L1 = np.sum(np.abs(y2 - y1int))
+    diff= np.abs(y2 - y1int)
+    L1 = np.sum(diff)
     
     #normalize by the number of points
     N_points = 1.0*len(y2)
@@ -189,12 +224,16 @@ def L1Diff(dataset_1, dataset_2, threshold=1.0):
         success = False
     else:
         success = True
-    
-    return L1, success
-    
+
+    if (details):
+        #save detailed results to dictionary if requested
+        results={'diff':diff,'x':x2,'y1int':y1int,'y2':y2}
+        return L1, success, results
+    else:
+        return L1, success
     
 
-def KS_test(dataset_1, dataset_2, threshold=1.0):
+def KS_test(dataset_1, dataset_2, threshold=1.0, details=False):
     """
     compute the K-S statistic for 2 datasets (CDFs)
     
@@ -250,7 +289,8 @@ def KS_test(dataset_1, dataset_2, threshold=1.0):
         y1int=y1
     
     #compute the K-S statistic
-    KS = np.max(np.abs(y2-y1int))
+    diff= np.abs(y2-y1int)
+    KS = np.max(diff)
     
     #return result
     if (KS > threshold) or (np.isnan(KS)):
@@ -258,6 +298,53 @@ def KS_test(dataset_1, dataset_2, threshold=1.0):
     else:
         success = True
     
-    return KS, success
+    if (details):
+        #save detailed results to dictionary if requested
+        results={'diff':diff,'x':x2,'y1int':y1int,'y2':y2}
+        return KS, success, results
+    else:
+        return KS, success
     
     
+def write_summary_details(results, filename, method='diff',comment=None):
+    """
+    write summary_details data file
+
+    Parameters
+    ----------
+    results : dictionary
+
+    filename : string
+
+    comment : string
+    """
+
+    #save results to file
+
+    if len(results)>0:
+        f = open(filename, 'w')
+        if comment:
+            f.write('# {0}\n'.format(comment))
+        #write header and results depending on dict contents
+        f.write('# {0}\n'.format('Columns are: '+method+', x, y1, y2, (e1), (e2)'))
+        if ('e1int' in results and 'e2' in results):
+            for b, h, hn, hx, en, ex in zip(*(results[k] for k in ['diff','x','y1int','y2','e1int','e2'])):
+                f.write("%13.6e %13.6e %13.6e %13.6e %13.6e %13.6e\n" % (b, h, hn, hx, en, ex))
+        elif ('e1int' in results):
+            for b, h, hn, hx, en in zip(*(results[k] for k in ['diff','x','y1int','y2','e1int'])):
+                f.write("%13.6e %13.6e %13.6e %13.6e %13.6e\n" % (b, h, hn, hx, en))
+        elif ('e2' in results):
+            for b, h, hn, hx, en in zip(*(results[k] for k in ['diff','x','y1int','y2','e2'])):
+                f.write("%13.6e %13.6e %13.6e %13.6e %13.6e\n" % (b, h, hn, hx, en))
+        else:
+            for b, h, hn, hx in zip(*(results[k] for k in ['diff','x','y1int','y2'])):
+                f.write("%13.6e %13.6e %13.6e %13.6e\n" % (b, h, hn, hx))
+        #f.write('Check average '+method+': %13.6e\n' %(np.sum(results['diff'])/len(results['y2'])) )
+        f.close()
+    else:
+        msg = ('Empty summary_details dict')
+        warn(msg)
+    
+
+
+