1a08e6b231b6a372406dfa6ca6473c9174c6dddc
Yao-Yuan Mao <yymao.astro@gmail.com>
2017-02-12 10:11:43 -0800

    fix permission

--- www/fix_permission.sh
+++ www/fix_permission.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-python -m py_compile utils/*.py
+python -m py_compile utils/*.py descqa/*.py
 
-chmod o+r   config
-chmod o+rx  *.cgi
-chmod o+rx  style utils viewer descqa descqa/templates
-chmod o+rwx cache
-chmod o+r   viewer/*
-chmod o+rx  viewer/*.cgi
-chmod o+r   style/*
-chmod o+r   utils/*
-chmod o+r   descqa/*
-chmod o+rx  descqa/*.py
-chmod o+r   descqa/templates/*
+chmod o+r    config
+chmod o+rx   *.cgi
+chmod o+rx   style utils viewer descqa descqa/templates
+chmod o+rwx  cache
+chmod o+r    viewer/*
+chmod o+rx   viewer/*.cgi
+chmod o+r    style/*
+chmod o+r    utils/*
+chmod -R o+r descqa/*
+chmod o+r    ../config_*.py
+ln -sf ../config_*.py ./
