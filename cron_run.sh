4d184772f22dd4e3cced681374694aa4decfcf19
Yao-Yuan Mao <yymao.astro@gmail.com>
2017-09-21 10:05:47 -0700

    update path

--- cron_run.sh
+++ cron_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
 . /opt/modules/default/etc/modules.sh
-DESCQA=/global/projecta/projectdirs/lsst/groups/CS/descqa
-cd ${DESCQA}/src/v2
-./run_master.sh -m "full run" 1>>${DESCQA}/run/cron.log 2>&1
+cd /global/projecta/projectdirs/lsst/www/descqa/v2
+./run_master.sh -m "full run" 1>>/global/projecta/projectdirs/lsst/groups/CS/descqa/run/v2/cron.log 2>&1
+
