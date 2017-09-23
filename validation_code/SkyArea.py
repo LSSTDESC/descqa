8f592d4de3cb235ab0e10d5e770ca4642c1e0dcc
Yao-Yuan Mao <yymao.astro@gmail.com>
2017-09-22 20:07:37 -0700

    add SkyArea test

---
+++ validation_code/SkyArea.py
@@ -0,0 +1,42 @@
+import os
+import numpy as np
+import healpy as hp
+from ValidationTest import BaseValidationTest, TestResult, plt
+
+__all__ = ['SkyArea']
+
+class SkyArea(BaseValidationTest):
+    """
+    validation test to show sky area
+    """
+    def __init__(self, nside=8, **kwargs):
+        assert hp.isnsideok(nside), '`nside` value {} not correct'.format(nside)
+        self._nside = nside
+
+    @staticmethod
+    def _save_quantities(catalog_name, quantities, filename):
+        quantities = list(quantities)
+        quantities.sort()
+        with open(filename, 'w') as f:
+            f.write('# ' + catalog_name + '\n')
+            for q in quantities:
+                f.write(str(q))
+                f.write('\n')
+
+    def run_validation_test(self, galaxy_catalog, catalog_name, base_output_dir):
+        if not galaxy_catalog.has_quantities(['ra_true', 'dec_true']):
+            return TestResult(skipped=True)
+
+        pixels = set()
+        for d in galaxy_catalog.get_quantities(['ra_true', 'dec_true'], return_iterator=True):
+            pixels.update(hp.ang2pix(self._nside, d['ra_true'], d['dec_true'], lonlat=True))
+
+        hp_map = np.empty(hp.nside2npix(self._nside))
+        hp_map.fill(hp.UNSEEN)
+        hp_map[list(pixels)] = 0
+
+        hp.mollview(hp_map, title=catalog_name, coord='C', cbar=None)
+        plt.savefig(os.path.join(base_output_dir, 'skymap.png'))
+        plt.close()
+        return TestResult(0, passed=True)
+
