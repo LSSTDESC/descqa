eda982f3a187436af2af8ecbf7052b00fb1ed153
Yao-Yuan Mao <yymao.astro@gmail.com>
2017-11-09 14:59:55 -0800

    permission fix

--- www/fix_permission.sh
+++ www/fix_permission.sh
@@ -7,5 +7,5 @@ mkdir -p cache
 chmod o+r    ../index.html .htaccess
 chmod o+rx   *.cgi
 chmod o+rwx  cache
-chmod o+rx   descqaweb style . ..
+chmod o+rx   descqaweb descqaweb/templates descqaweb/view_bigboard style . ..
 chmod -R o+r descqaweb style
