f81cabe3f68e2a8d0a7f8a623ca745a1bdc24153
Yao-Yuan Mao <yymao.astro@gmail.com>
2017-11-09 21:12:05 -0800

    final update

--- www/descqaweb/config.py
+++ www/descqaweb/config.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 __all__ = ['site_title', 'root_dir', 'run_per_page']
 
 site_title   = 'DESCQA: LSST DESC Quality Assurance'
-root_dir     = '/global/projecta/projectdirs/lsst/groups/CS/descqa/run/v1'
+root_dir     = '/global/projecta/projectdirs/lsst/groups/CS/descqa/run/v2'
 run_per_page = 20
