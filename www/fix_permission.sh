8b89ed5c2f7988f52bb5c4c7dd6fb290454542cc
Yao-Yuan Mao <yymao.astro@gmail.com>
2017-11-11 19:43:07 -0800

    update fix_permission.sh

--- www/fix_permission.sh
+++ www/fix_permission.sh
@@ -3,6 +3,8 @@ set -e
 
 /global/common/cori/contrib/lsst/apps/anaconda/py3-envs/DESCQA/bin/python -c "import descqaweb"
 
-chmod o+rx descqaweb descqaweb/templates style index.cgi . ..
-chmod -R o+r .
+chmod o+rx index.cgi
+chmod -R o+rX .
+chmod o+r ../index.html
+chmod o+rx ..
 
