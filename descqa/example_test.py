8cae33e5d6c4bbeffa0ea765df3b233ac356713c
Yao-Yuan Mao <yymao.astro@gmail.com>
2017-11-23 19:35:58 -0500

    bug fix

--- descqa/example_test.py
+++ descqa/example_test.py
@@ -21,7 +21,7 @@ class ExampleTest(BaseValidationTest):
 
         # load validation data
         with open(os.path.join(self.data_dir, 'README.md')) as f:
-            self.data = f.readline().strip()
+            self.validation_data = f.readline().strip()
 
         # prepare summary plot
         self.summary_fig, self.summary_ax = plt.subplots()
@@ -33,18 +33,24 @@ class ExampleTest(BaseValidationTest):
         if not galaxy_catalog.has_quantities(['ra', 'dec']):
             return TestResult(skipped=True, summary='do not have needed quantities')
 
+        data = np.random.rand(10) #do your calculation with galaxy_catalog
+
         fig, ax = plt.subplots()
 
         for ax_this in (ax, self.summary_ax):
-            ax_this.plot(np.random.rand(10), label=catalog_name)
+            ax_this.plot(data, label=catalog_name)
+            ax_this.text(0.05, 0.95, self.validation_data)
 
+        ax.legend()
         fig.savefig(os.path.join(base_output_dir, 'plot.png'))
         plt.close(fig)
-        score = np.random.rand()
+
+        score = data[0] #calculate your summary statistics
         return TestResult(score, passed=True)
 
 
     def generate_summary(self, catalog_name_list, base_output_dir):
 
+        self.summary_ax.legend()
         self.summary_fig.savefig(os.path.join(base_output_dir, 'summary.png'))
         plt.close(self.summary_fig)
