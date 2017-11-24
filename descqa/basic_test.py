b5fb42d00cc31580d5d3d527202a4a939dfd3efc
Yao-Yuan Mao <yymao.astro@gmail.com>
2017-11-23 19:28:29 -0500

    bug fix

--- descqa/basic_test.py
+++ descqa/basic_test.py
@@ -4,7 +4,7 @@ from builtins import str
 import yaml
 from .base import BaseValidationTest, TestResult
 
-__all__ = ['ListAvailableQuantities']
+__all__ = ['ListAvailableQuantities', 'SkyArea']
 
 class ListAvailableQuantities(BaseValidationTest):
     """
@@ -26,4 +26,31 @@ class ListAvailableQuantities(BaseValidationTest):
         with open(os.path.join(base_output_dir, 'config.yaml'), 'w') as f:
             f.write(yaml.dump(galaxy_catalog.get_catalog_info(), default_flow_style=False))
             f.write('\n')
+        return TestResult(0, passed=True)
+
+
+class SkyArea(BaseValidationTest):
+    """
+    validation test to show sky area
+    """
+    def __init__(self, **kwargs):
+        self.nside = kwargs.get('nside', 16)
+        assert hp.isnsideok(self.nside), '`nside` value {} not correct'.format(self.nside)
+
+
+    def run_validation_test(self, galaxy_catalog, catalog_name, base_output_dir):
+        if not galaxy_catalog.has_quantities(['ra_true', 'dec_true']):
+            return TestResult(skipped=True)
+
+        pixels = set()
+        for d in galaxy_catalog.get_quantities(['ra_true', 'dec_true'], return_iterator=True):
+            pixels.update(hp.ang2pix(self.nside, d['ra_true'], d['dec_true'], lonlat=True))
+
+        hp_map = np.empty(hp.nside2npix(self.nside))
+        hp_map.fill(hp.UNSEEN)
+        hp_map[list(pixels)] = 0
+
+        hp.mollview(hp_map, title=catalog_name, coord='C', cbar=None)
+        plt.savefig(os.path.join(base_output_dir, 'skymap.png'))
+        plt.close()
         return TestResult(0, passed=True)
