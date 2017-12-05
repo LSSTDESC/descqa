0df58c14c909a845e602b63172e8c7edd84cbdb4
evevkovacs <kovacs@anl.gov>
2017-12-05 15:49:02 -0800

    Updated basic_test to include value ranges

--- descqa/basic_test.py
+++ descqa/basic_test.py
@@ -6,6 +6,7 @@ import numpy as np
 import healpy as hp
 from .base import BaseValidationTest, TestResult
 from .plotting import plt
+from collections import defaultdict
 
 __all__ = ['ListAvailableQuantities', 'SkyArea']
 
@@ -13,17 +14,68 @@ class ListAvailableQuantities(BaseValidationTest):
     """
     validation test to list all available quantities
     """
-    @staticmethod
-    def _save_quantities(catalog_name, quantities, filename):
+    def __init__(self, **kwargs):
+        
+        self.kwargs = kwargs
+        self.calc_min_max = kwargs.get('calc_min_max', False)
+
+    def _save_quantities(self, catalog_name, quantities, filename):
         quantities = list(quantities)
         quantities.sort()
+        maxlen = max((len(q) for q in quantities))
+        qtype = 'native' if filename.find('native')!=-1 else 'GCR'
         with open(filename, 'w') as f:
-            f.write('# ' + catalog_name + '\n')
+            if self.calc_min_max:
+                f.write('{} {} {} \n'.format('# ' + catalog_name.ljust(maxlen), 'Minimum'.rjust(13), 'Maximum'.rjust(13)))
+            else:
+                f.write('# ' + catalog_name + '\n')
             for q in quantities:
-                f.write(str(q))
+                if self.calc_min_max:
+                    f.write('{} {:13.4g} {:13.4g} '.format(q.ljust(maxlen), self.ranges[qtype + '-min'].get(q, np.nan), self.ranges[qtype + '-max'].get(q, np.nan)))
+                else:
+                    f.write(str(q))
                 f.write('\n')
 
+    def _get_data_ranges(self, catalog_instance):
+
+        self.ranges = defaultdict(dict)
+
+        d_min = self.ranges['GCR-min']
+        d_max = self.ranges['GCR-max']
+        quantities = catalog_instance.list_all_quantities()
+        for data in catalog_instance.get_quantities(quantities, return_iterator=True):
+            for q in quantities:
+                if data[q].dtype.char in 'bBiulfd':
+                    d_min[q] = min(np.nanmin(data[q]), d_min.get(q, np.inf))
+                    d_max[q] = max(np.nanmax(data[q]), d_max.get(q, -np.inf))
+                
+        native_quantities = catalog_instance.list_all_native_quantities()
+        quantities_needed=[]
+        #check for name collisions and add native quantity
+        for q in native_quantities:
+            if q in quantities:
+                catalog_instance.add_quantity_modifier(q + '_native', q)
+                quantities_needed.append(q + '_native')
+            else:
+                quantities_needed.append(q)
+
+        d_min = self.ranges['native-min']
+        d_max = self.ranges['native-max']
+        for data in catalog_instance.get_quantities(quantities_needed, return_iterator=True):
+            for qx in quantities_needed:
+                q = qx.replace('_native','')
+                if data[qx].dtype.char in 'bBiulfd':
+                    d_min[q] = min(np.nanmin(data[qx]), d_min.get(q, np.inf))
+                    d_max[q] = max(np.nanmax(data[qx]), d_max.get(q, -np.inf))
+
+        #clean_up q_native added quantities
+        for qx in quantities_needed:
+            if qx.find('_native')!=-1:
+                catalog_instance.del_quantity_modifier(qx)
+
     def run_on_single_catalog(self, catalog_instance, catalog_name, output_dir):
+        if self.calc_min_max:
+            self._get_data_ranges(catalog_instance)
         self._save_quantities(catalog_name, catalog_instance.list_all_quantities(), os.path.join(output_dir, 'quantities.txt'))
         self._save_quantities(catalog_name, catalog_instance.list_all_native_quantities(), os.path.join(output_dir, 'native_quantities.txt'))
         with open(os.path.join(output_dir, 'config.yaml'), 'w') as f:
