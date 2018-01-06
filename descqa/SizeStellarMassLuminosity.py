f910ad141720ec1470e9e55e3442f160dcb6c61e
Vinu Vikraman <vvinuv@gmail.com>
2018-01-05 21:47:00 -0800

    Preliminary version of size-luminosity validation script and added Table 5 of van der Wal et al 2014

---
+++ descqa/SizeStellarMassLuminosity.py
@@ -0,0 +1,135 @@
+from __future__ import print_function, division, unicode_literals, absolute_import
+import os
+import numpy as np
+from GCR import GCRQuery
+from scipy import interpolate 
+
+from .base import BaseValidationTest, TestResult
+from .plotting import plt
+from .utils import generate_uniform_random_ra_dec_footprint, get_healpixel_footprint, generate_uniform_random_dist
+
+
+__all__ = ['SizeStellarMassLuminosity']
+
+
+def redshift2dist(cosmology):
+    z = np.arange(0, 5.1, 0.5)
+    comov_d = cosmology.comoving_distance(z).to('kpc').value
+    spl = interpolate.splrep(z, comov_d)   
+    return spl
+
+
+class SizeStellarMassLuminosity(BaseValidationTest):
+    """
+    Validation test of 2pt correlation function
+    """
+    _C = 299792.458
+
+    def __init__(self, **kwargs):
+        self.possible_mag_fields = kwargs['possible_mag_fields']
+        self.data_label = kwargs['data_label']
+        self.z_bins = kwargs['z_bins']
+        self.output_filename_template = kwargs['output_filename_template']
+        self.label_template = kwargs['label_template']
+        self.fig_xlabel = kwargs['fig_xlabel']
+        self.fig_ylabel = kwargs['fig_ylabel']
+
+
+        validation_filepath = os.path.join(self.data_dir, kwargs['data_filename'])
+        self.validation_data = np.genfromtxt(validation_filepath)
+    
+    @staticmethod
+    def ConvertAbsMagLuminosity(AbsM, band):
+        '''AbsM: absolute magnitude, band: filter'''
+        if type(AbsM) is list or type(AbsM) is np.ndarray:
+            AbsM = np.array(AbsM)
+
+        bands = {'U':5.61, 'B':5.48, 'V':4.83, 'R':4.42, 'I':4.08, 
+                 'J':3.64, 'H':3.32, 'K':3.28, 'g':5.33, 'r':4.67, 
+                 'i':4.48, 'z':4.42, 'F300W':6.09, 'F450W':5.32, 'F555W':4.85, 
+                 'F606W':4.66, 'F702W':4.32, 'F814W':4.15, 'CFHT_U':5.57, 
+                 'CFHT_B':5.49, 'CFHT_V':4.81, 'CFHT_R':4.44, 'CFHT_I':4.06, 
+                 'NIRI_J':3.64, 'NIRI_H':3.33, 'NIRI_K':3.29}
+
+        if band in bands.keys():
+            AbsSun = bands[band]
+        else:
+            raise ValueError('Filter not implemented')
+
+        logL = (AbsSun - AbsM) / 2.5 #unit of sun
+        L = 10**logL
+        return L
+
+    def run_on_single_catalog(self, catalog_instance, catalog_name, output_dir):
+        '''
+        Loop over magnitude cuts and make plots
+        '''
+        # load catalog data
+        spl = redshift2dist(catalog_instance.cosmology)
+        
+        colnames = dict()
+        #colnames['z'] = catalog_instance.first_available('redshift')
+        #colnames['size'] = catalog_instance.first_available('size')
+        colnames['z'] = 'redshift'
+        colnames['size'] = 'size'
+        colnames['mag'] = catalog_instance.first_available(*self.possible_mag_fields)
+        #print('colnames ', colnames)
+        if not all(v for v in colnames.values()):
+            return TestResult(skipped=True, summary='Missing requested quantities')
+        #Check whether the columns are finite or not
+        filters = [(np.isfinite, c) for c in colnames.values()]
+        #zz = [z_bin['z_max'] for z_bin in self.z_bins]
+        #print('z ', zz)
+        #Select objects within maximum and minimum redshift of the all the bins
+        filters.extend((
+            '{} < {}'.format(colnames['z'], max(z_bin['z_max'] for z_bin in self.z_bins)),
+            '{} >= {}'.format(colnames['z'], min(z_bin['z_min'] for z_bin in self.z_bins)),
+        ))
+        catalog_data = catalog_instance.get_quantities(list(colnames.values()), filters=filters)
+        catalog_data = {k: catalog_data[v] for k, v in colnames.items()}
+
+        fig, axes = plt.subplots(2,3, figsize=(6, 9), sharex=True, sharey=True)
+        try:
+            i = 0
+            j = 0
+            for z_bin in self.z_bins:
+                ax = axes[i,j]
+                
+                # filter catalog data for this bin
+                filters = [
+                    'z < {}'.format(z_bin['z_max']),
+                    'z >= {}'.format(z_bin['z_min']),
+                ]
+
+                catalog_data_this = GCRQuery(*filters).filter(catalog_data)
+                size_kpc = catalog_data_this['size'] * interpolate.splev(catalog_data_this['z'], spl) / (1 + catalog_data_this['z'])
+
+                del catalog_data_this
+
+                z_mean = (z_bin['z_max'] + z_bin['z_min']) / 2.
+                validation_this = self.validation_data[(self.validation_data[:,0] < z_mean + 0.02) & (self.validation_data[:,0] > z_mean - 0.02)]
+
+                ax.plot(validation_this[:,1], validation_this[:, 2], label=self.label_template.format(z_bin['z_min'], z_bin['z_max']))
+                ax.fill_between(validation_this[:,1], validation_this[:,3], validation_this[:,4], lw=0, alpha=0.2)
+                #ax.errorbar(xi_rad, xi*scale_wp, xi_sig*scale_wp, marker='o', ls='', c=color)
+                j += 1
+                if j > 2:
+                    j = 0
+                    i += 1
+
+                ax.legend(loc='best')
+            fig.add_subplot(111, frameon=False)
+            # hide tick and tick label of the big axes
+            plt.tick_params(labelcolor='none', top='off', bottom='off', left='off', right='off')
+            plt.grid(False)
+            plt.xlabel(self.fig_xlabel)
+            plt.ylabel(self.fig_ylabel)
+            fig.subplots_adjust(hspace=0, wspace=0)
+            fig.suptitle('{} vs. {}'.format(catalog_name, self.data_label), fontsize='medium')
+        finally:
+            #fig.savefig(os.path.join(output_dir, '{:s}.png'.format('t')), bbox_inches='tight')
+            fig.savefig(os.path.join('./', '{:s}.png'.format('t')), bbox_inches='tight')
+            plt.close(fig)
+        
+        #TODO: calculate summary statistics
+        return TestResult(0, passed=True)
