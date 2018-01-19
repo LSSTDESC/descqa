ec333df61d54e8559c6382cb6b76f9c8772bb1ad
Yao-Yuan Mao <yymao.astro@gmail.com>
2018-01-19 14:25:55 -0800

    add a line break

--- descqaweb/config.py
+++ descqaweb/config.py
@@ -13,7 +13,7 @@ github_url = 'https://github.com/lsstdesc/descqa'
 
 general_info = '''
 This is DESCQA v2. You can also visit the previous version, <a class="everblue" href="https://portal.nersc.gov/project/lsst/descqa/v1/">DESCQA v1</a>.
-<br>
+<br><br>
 The DESCQA framework executes validation tests on mock galaxy catalogs.
 These tests and catalogs are contributed by LSST DESC collaborators.
 See <a href="https://arxiv.org/abs/1709.09665" target="_blank">the DESCQA paper</a> for more information.
