a2916f3f4591d64da14cbe656824504a0705a027
Melanie Simet <melanie.simet@gmail.com>
2018-02-23 13:58:09 -0800

    Fix whitespace errors

--- descqa/SizeDistribution.py
+++ descqa/SizeDistribution.py
@@ -45,8 +45,8 @@ class SizeDistribution(BaseValidationTest):
         sizes = catalog_data[key]
         good_data_mask = np.logical_not(np.logical_or(np.isinf(sizes), np.isnan(sizes)))
         sizes = sizes[good_data_mask]
-        non_neg_mask = sizes>0
-        if np.sum(non_neg_mask)>0:
+        non_neg_mask = sizes > 0
+        if np.sum(non_neg_mask) > 0:
             print('Warning: some sizes were negative or zero; these are being masked')
             sizes = sizes[non_neg_mask]
         min_sizes = np.min(sizes)
@@ -56,18 +56,18 @@ class SizeDistribution(BaseValidationTest):
         # Things seem to be roughly linear where N(size)>0.5*Ntot so use those points.
         # Get ~20 points for the line fit, but compute the whole graph
         median = np.median(sizes)
-        n_bins = int(20*(max_sizes-min_sizes)/(median-min_sizes)) 
+        n_bins = int(20*(max_sizes-min_sizes)/(median-min_sizes))
         N, bin_edges = np.histogram(sizes, bins=n_bins)
         sumM = np.histogram(sizes, weights=sizes, bins=bin_edges)[0]
         sumM2 = np.histogram(sizes, weights=sizes**2, bins=bin_edges)[0]
         size_pts = get_opt_binpoints(N, sumM, sumM2, bin_edges)
         diff = size_pts[1:] - size_pts[:-1]
-        if not np.all(diff>=0):
+        if not np.all(diff >= 0):
             # Sparsely populated bins sometimes cause problems for
             # get_opt_binpoints; replace with the dumb solution
             size_pts = 0.5*(bin_edges[:-1]+bin_edges[1:])
 
-        mask = size_pts<median
+        mask = size_pts < median
 
         # Normalize so we can compare datasets of different sizes
         cumul_N_norm = np.array(
@@ -78,15 +78,15 @@ class SizeDistribution(BaseValidationTest):
         # Compute the slope for the validation dataset in this size range.
         # Copy the validation dataset so we can play with it
         validation_data = self.validation_data.copy()
-        validation_mask = (validation_data[:,0] > min_sizes) & (validation_data[:,0]<median)
-        validation_data[:,1] /= validation_data[validation_mask,1][0]
+        validation_mask = (validation_data[:, 0] > min_sizes) & (validation_data[:, 0] < median)
+        validation_data[:, 1] /= validation_data[validation_mask, 1][0]
         validation_slope, _ = np.polyfit(validation_data[validation_mask, 0],
-                                                         validation_data[validation_mask, 1], 1)
+                                         validation_data[validation_mask, 1], 1)
 
         # plot a histogram of sizes. This is easier to see as log(sizes) so do that.
         fig, (hist_ax, cumul_ax) = plt.subplots(1, 2)
         fig.subplots_adjust(wspace=0.4)
-        hist_ax.hist(np.log10(sizes), color=catalog_color, edgecolor='black', alpha=0.75, 
+        hist_ax.hist(np.log10(sizes), color=catalog_color, edgecolor='black', alpha=0.75,
                      normed=True, bins=20)
         hist_ax.set_xlabel("Log10({})".format(key))
         hist_ax.set_ylabel("dN/d log({})".format(key))
@@ -96,7 +96,7 @@ class SizeDistribution(BaseValidationTest):
         cumul_ax.plot(size_pts[mask], (data_intercept+data_slope*size_pts[mask]), color='gray')
         cumul_ax.set_xscale('log')
         cumul_ax.text(0.95, 0.96,
-                      'validation=${:.3f}$\nslope=${:.3f}$'.format(validation_slope, data_slope), 
+                      'validation=${:.3f}$\nslope=${:.3f}$'.format(validation_slope, data_slope),
                       horizontalalignment='right', verticalalignment='top',
                       transform=cumul_ax.transAxes)
         cumul_ax.set_xlabel("{}".format(key))
@@ -105,7 +105,7 @@ class SizeDistribution(BaseValidationTest):
         with open(os.path.join(output_dir, 'size_distribution_{}.txt'.format(catalog_name)), 'w'
                  ) as f:
             f.write("# Slope, intercept\n")
-            f.write("%7f  %9f\n"%(data_slope, data_intercept)) 
+            f.write("%7f  %9f\n"%(data_slope, data_intercept))
 
         fig.savefig(os.path.join(output_dir, 'size_distribution_{}.png'.format(catalog_name)))
         plt.close(fig)
