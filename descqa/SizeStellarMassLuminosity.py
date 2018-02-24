a8b9f16baeac930a6f61b90e6cb5eb70522021b2
Yao-Yuan Mao <yymao.astro@gmail.com>
2018-02-23 21:28:25 -0500

    fix linting errors

--- descqa/SizeStellarMassLuminosity.py
+++ descqa/SizeStellarMassLuminosity.py
@@ -2,7 +2,7 @@ from __future__ import print_function, division, unicode_literals, absolute_impo
 import os
 import numpy as np
 from GCR import GCRQuery
-from scipy import interpolate 
+from scipy import interpolate
 from scipy.stats import binned_statistic
 
 from .base import BaseValidationTest, TestResult
@@ -15,7 +15,7 @@ __all__ = ['SizeStellarMassLuminosity']
 def redshift2dist(cosmology):
     z = np.arange(0, 5.1, 0.5)
     comov_d = cosmology.comoving_distance(z).to('kpc').value
-    spl = interpolate.splrep(z, comov_d)   
+    spl = interpolate.splrep(z, comov_d)
     return spl
 
 
@@ -26,6 +26,7 @@ class SizeStellarMassLuminosity(BaseValidationTest):
     _ARCSEC_TO_RADIAN = np.pi / 180. / 3600.
 
     def __init__(self, **kwargs):
+        #pylint: disable=W0231
         self.kwargs = kwargs
         self.observation = kwargs['observation']
         self.possible_mag_fields = kwargs['possible_mag_fields']
@@ -39,18 +40,17 @@ class SizeStellarMassLuminosity(BaseValidationTest):
 
         validation_filepath = os.path.join(self.data_dir, kwargs['data_filename'])
         self.validation_data = np.genfromtxt(validation_filepath)
-    
+
     @staticmethod
     def ConvertAbsMagLuminosity(AbsM, band):
         '''AbsM: absolute magnitude, band: filter'''
-        if type(AbsM) is list or type(AbsM) is np.ndarray:
-            AbsM = np.array(AbsM)
+        AbsM = np.asarray(AbsM)
 
-        bands = {'U':5.61, 'B':5.48, 'V':4.83, 'R':4.42, 'I':4.08, 
-                 'J':3.64, 'H':3.32, 'K':3.28, 'g':5.33, 'r':4.67, 
-                 'i':4.48, 'z':4.42, 'F300W':6.09, 'F450W':5.32, 'F555W':4.85, 
-                 'F606W':4.66, 'F702W':4.32, 'F814W':4.15, 'CFHT_U':5.57, 
-                 'CFHT_B':5.49, 'CFHT_V':4.81, 'CFHT_R':4.44, 'CFHT_I':4.06, 
+        bands = {'U':5.61, 'B':5.48, 'V':4.83, 'R':4.42, 'I':4.08,
+                 'J':3.64, 'H':3.32, 'K':3.28, 'g':5.33, 'r':4.67,
+                 'i':4.48, 'z':4.42, 'F300W':6.09, 'F450W':5.32, 'F555W':4.85,
+                 'F606W':4.66, 'F702W':4.32, 'F814W':4.15, 'CFHT_U':5.57,
+                 'CFHT_B':5.49, 'CFHT_V':4.81, 'CFHT_R':4.44, 'CFHT_I':4.06,
                  'NIRI_J':3.64, 'NIRI_H':3.33, 'NIRI_K':3.29}
 
         if band in bands.keys():
@@ -59,7 +59,6 @@ class SizeStellarMassLuminosity(BaseValidationTest):
             raise ValueError('Filter not implemented')
 
         logL = (AbsSun - AbsM) / 2.5 #unit of sun
-        L = 10**logL
         return logL
 
     def run_on_single_catalog(self, catalog_instance, catalog_name, output_dir):
@@ -68,7 +67,7 @@ class SizeStellarMassLuminosity(BaseValidationTest):
         '''
         # load catalog data
         spl = redshift2dist(catalog_instance.cosmology)
-        
+
         colnames = dict()
         colnames['z'] = 'redshift'
         colnames['mag'] = catalog_instance.first_available(*self.possible_mag_fields)
@@ -77,7 +76,7 @@ class SizeStellarMassLuminosity(BaseValidationTest):
         elif self.observation == 'twocomp':
             colnames['size_bulge'] = 'size_bulge_true'
             colnames['size_disk'] = 'size_disk_true'
-             
+
         if not all(v for v in colnames.values()):
             return TestResult(skipped=True, summary='Missing requested quantities')
         #Check whether the columns are finite or not
@@ -91,7 +90,7 @@ class SizeStellarMassLuminosity(BaseValidationTest):
         catalog_data = catalog_instance.get_quantities(list(colnames.values()), filters=filters)
         catalog_data = {k: catalog_data[v] for k, v in colnames.items()}
 
-        fig, axes = plt.subplots(2,3, figsize=(9, 6), sharex=True, sharey=True)
+        fig, axes = plt.subplots(2, 3, figsize=(9, 6), sharex=True, sharey=True)
         try:
             col = 0
             row = 0
@@ -105,7 +104,7 @@ class SizeStellarMassLuminosity(BaseValidationTest):
 
                 catalog_data_this = GCRQuery(*filters).filter(catalog_data)
                 if len(catalog_data_this['z']) == 0:
-                    continue 
+                    continue
                 z_mean = (z_bin['z_max'] + z_bin['z_min']) / 2.
                 output_filepath = os.path.join(output_dir, self.output_filename_template.format(z_bin['z_min'], z_bin['z_max']))
                 colors = ['r', 'b']
@@ -121,7 +120,7 @@ class SizeStellarMassLuminosity(BaseValidationTest):
 
                     validation_this = self.validation_data[(self.validation_data[:,0] < z_mean + 0.25) & (self.validation_data[:,0] > z_mean - 0.25)]
 
-                    ax.semilogy(validation_this[:,1], 10**validation_this[:, 2], label=self.label_template.format(z_bin['z_min'], z_bin['z_max']))
+                    ax.semilogy(validation_this[:,1], 10**validation_this[:,2], label=self.label_template.format(z_bin['z_min'], z_bin['z_max']))
                     ax.fill_between(validation_this[:,1], 10**validation_this[:,3], 10**validation_this[:,4], lw=0, alpha=0.2)
                     ax.errorbar(default_L_bins, binned_size_kpc, binned_size_kpc_err, marker='o', ls='')
                 elif self.observation == 'twocomp':
@@ -141,10 +140,10 @@ class SizeStellarMassLuminosity(BaseValidationTest):
                     validation_this = self.validation_data[(self.validation_data[:,0] < z_mean + 0.25) & (self.validation_data[:,0] > z_mean - 0.25)]
 
                     ax.text(11, 0.3, self.label_template.format(z_bin['z_min'], z_bin['z_max']))
-                    ax.semilogy(validation_this[:,1], validation_this[:, 2], label='Bulge', color=colors[0])
-                    ax.fill_between(validation_this[:,1], validation_this[:, 2] + validation_this[:,4], validation_this[:, 2] - validation_this[:,4], lw=0, alpha=0.2, facecolor=colors[0])
-                    ax.semilogy(validation_this[:,1] + 0.2, validation_this[:, 3], label='Disk', color=colors[1])
-                    ax.fill_between(validation_this[:,1] + 0.2, validation_this[:, 3] + validation_this[:,5], validation_this[:, 3] - validation_this[:,5], lw=0, alpha=0.2, facecolor=colors[1])
+                    ax.semilogy(validation_this[:,1], validation_this[:,2], label='Bulge', color=colors[0])
+                    ax.fill_between(validation_this[:,1], validation_this[:,2] + validation_this[:,4], validation_this[:,2] - validation_this[:,4], lw=0, alpha=0.2, facecolor=colors[0])
+                    ax.semilogy(validation_this[:,1] + 0.2, validation_this[:,3], label='Disk', color=colors[1])
+                    ax.fill_between(validation_this[:,1] + 0.2, validation_this[:,3] + validation_this[:,5], validation_this[:,3] - validation_this[:,5], lw=0, alpha=0.2, facecolor=colors[1])
 
                     ax.errorbar(default_L_bins, binned_bulgesize_kpc, binned_bulgesize_kpc_err, marker='o', ls='', c=colors[0])
                     ax.errorbar(default_L_bins+0.2, binned_disksize_kpc, binned_disksize_kpc_err, marker='o', ls='', c=colors[1])
@@ -171,6 +170,6 @@ class SizeStellarMassLuminosity(BaseValidationTest):
         finally:
             fig.savefig(os.path.join(output_dir, '{:s}.png'.format(self.test_name)), bbox_inches='tight')
             plt.close(fig)
-        
+
         #TODO: calculate summary statistics
-        return TestResult(0, passed=True)
+        return TestResult(inspect_only=True)
