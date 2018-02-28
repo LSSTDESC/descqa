5f95ed14b44fe66bf849862d662b4149ec207283
Sam Smith <nsevilla@gmail.com>
2018-02-28 04:27:20 -0800

    More refined version, for protoDC2 only, any band combination

--- descqa/CheckColors.py
+++ descqa/CheckColors.py
@@ -8,54 +8,65 @@ __all__ = ['CheckColors']
 
 class CheckColors(BaseValidationTest):
     """
-    An example validation test
+    Inspection test to represent 2D color plots
     """
-    def __init__(self, **kwargs):
+    def __init__(self, xcolor='ri', ycolor='gr', magtype = 'mag_lsst', **kwargs):
 
         # load test config options
         self.kwargs = kwargs
-        ##self.option1 = kwargs.get('option1', 'option1_default')
-        ##self.option2 = kwargs.get('option2', 'option2_default')
         self.test_name = kwargs.get('test_name', 'CheckColors')
-
-        # load validation data
-        ##with open(os.path.join(self.data_dir, 'README.md')) as f:
-        ##    self.validation_data = f.readline().strip()
-
+        self.possible_mag_fields = ('mag_{}_lsst',
+                                    'mag_{}_sdss',
+                                    'mag_{}_des',
+                                    'mag_true_{}_lsst',
+                                    'mag_true_{}_sdss',
+                                    'mag_true_{}_des',
+                                   )
+        self.magtype = magtype
         # prepare summary plot
+        if len(xcolor)!=2 or len(ycolor)!=2:
+            print('Warning: color string is longer than 2 characters. Only first and last bands will be used.')	 
+        if magtype == 'mag_lsst':
+            self.xcolor1 = 'mag_{}_lsst'.format(xcolor[0])
+            self.xcolor2 = 'mag_{}_lsst'.format(xcolor[-1])
+            self.ycolor1 = 'mag_{}_lsst'.format(ycolor[0])
+            self.ycolor2 = 'mag_{}_lsst'.format(ycolor[-1])
+        else:
+            raise ValueError('Magnitude type {} not implemented'.format(magtype))
+            sys.exit()
+
         self.summary_fig, self.summary_ax = plt.subplots()
 
 
     def post_process_plot(self, ax):
-        #ax.text(0.05, 0.95, self.validation_data)
-        ax.text(-0.25, 1.80, 'g-r vs r-i')
-        #ax.legend()
+        #ax.text(-0.25, 1.80, '{}-{} vs {}-{}'.format(self.ycolor1,self.ycolor2,self.xcolor1,self.xcolor2))
+        ax.set_title('Color inspection for {}'.format(self.magtype))
 
     def run_on_single_catalog(self, catalog_instance, catalog_name, output_dir):
 
         # check if needed quantities exist
-        if not catalog_instance.has_quantities(['mag_g_lsst', 'mag_r_lsst','mag_i_lsst']):
-            return TestResult(skipped=True, summary='do not have needed quantities')
+        if not catalog_instance.has_quantities([self.xcolor1,self.xcolor2,self.ycolor1,self.ycolor2]):
+            
+            return TestResult(skipped=True, summary='Missing magnitudes in catalog')
 
-        #data = np.random.rand(10) #do your calculation with catalog_instance
-        data = catalog_instance.get_quantities(['redshift','mag_u_lsst','mag_g_lsst','mag_r_lsst','mag_i_lsst','mag_z_lsst'])
-        grcolor = data['mag_g_lsst'] - data['mag_r_lsst']
-        ricolor = data['mag_r_lsst'] - data['mag_i_lsst']
+        data = catalog_instance.get_quantities([self.xcolor1,self.xcolor2,self.ycolor1,self.ycolor2])
+        xcolor = data[self.xcolor1] - data[self.xcolor2]
+        ycolor = data[self.ycolor1] - data[self.ycolor2]
 
         fig, ax = plt.subplots()
 
         for ax_this in (ax, self.summary_ax):
             #ax_this.plot(data, label=catalog_name)
-            ax_this.set_xlabel('r-i',fontsize=20)		
-            ax_this.set_ylabel('g-r',fontsize=20)
-            ax_this.hexbin(ricolor,grcolor,gridsize=(100),cmap='GnBu',mincnt=1,bins='log')
+            ax_this.set_xlabel('{} - {}'.format(self.xcolor1,self.xcolor2),fontsize=14,labelpad=2)		
+            ax_this.set_ylabel('{} - {}'.format(self.ycolor1,self.ycolor2),fontsize=14,labelpad=2)
+            ax_this.hexbin(xcolor,ycolor,gridsize=(100),cmap='GnBu',mincnt=1,bins='log')
 
         self.post_process_plot(ax)
         fig.savefig(os.path.join(output_dir, 'colors.png'))
         plt.close(fig)
 
-        score = data[0] #calculate your summary statistics
-        return TestResult(score, passed=True)
+        #score = data[0] #calculate your summary statistics
+        return TestResult(inspect_only=True)
 
 
     def conclude_test(self, output_dir):
