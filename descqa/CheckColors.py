63a2023d1427deed02d3df779409baf8756960f9
Sam Smith <nsevilla@gmail.com>
2018-03-02 04:10:20 -0800

    First complete version of CheckColors

--- descqa/CheckColors.py
+++ descqa/CheckColors.py
@@ -10,7 +10,7 @@ class CheckColors(BaseValidationTest):
     """
     Inspection test to represent 2D color plots
     """
-    def __init__(self, xcolor='ri', ycolor='gr', magtype = 'mag_lsst', **kwargs):
+    def __init__(self, xcolor='ri', ycolor='gr', **kwargs):
 
         # load test config options
         self.kwargs = kwargs
@@ -18,58 +18,63 @@ class CheckColors(BaseValidationTest):
         self.possible_mag_fields = ('mag_{}_lsst',
                                     'mag_{}_sdss',
                                     'mag_{}_des',
+                                    'mag_{}_stripe82',
                                     'mag_true_{}_lsst',
                                     'mag_true_{}_sdss',
                                     'mag_true_{}_des',
-                                   )
-        self.magtype = magtype
+                                    'Mag_true_{}_des_z01',
+                                    'Mag_true_{}_sdss_z01'
+                                    )
         # prepare summary plot
         if len(xcolor)!=2 or len(ycolor)!=2:
             print('Warning: color string is longer than 2 characters. Only first and last bands will be used.')	 
-        if magtype == 'mag_lsst':
-            self.xcolor1 = 'mag_{}_lsst'.format(xcolor[0])
-            self.xcolor2 = 'mag_{}_lsst'.format(xcolor[-1])
-            self.ycolor1 = 'mag_{}_lsst'.format(ycolor[0])
-            self.ycolor2 = 'mag_{}_lsst'.format(ycolor[-1])
-        else:
-            raise ValueError('Magnitude type {} not implemented'.format(magtype))
-            sys.exit()
+        #if magtype == 'mag_lsst':
+        self.xcolor1 = xcolor[0]
+        self.xcolor2 = xcolor[-1]
+        self.ycolor1 = ycolor[0]
+        self.ycolor2 = ycolor[-1]
+        #else:
+        #    raise ValueError('Magnitude type {} not implemented'.format(magtype))
+        #    sys.exit()
 
         self.summary_fig, self.summary_ax = plt.subplots()
 
 
-    def post_process_plot(self, ax):
+    def post_process_plot(self, ax, name):
         #ax.text(-0.25, 1.80, '{}-{} vs {}-{}'.format(self.ycolor1,self.ycolor2,self.xcolor1,self.xcolor2))
-        ax.set_title('Color inspection for {}'.format(self.magtype))
+        ax.set_title('Color inspection for {}'.format(name))
 
     def run_on_single_catalog(self, catalog_instance, catalog_name, output_dir):
 
-        # check if needed quantities exist
-        if not catalog_instance.has_quantities([self.xcolor1,self.xcolor2,self.ycolor1,self.ycolor2]):
-            
-            return TestResult(skipped=True, summary='Missing magnitudes in catalog')
+        for mag_field in self.possible_mag_fields:
+            # check if needed quantities exist
+            xcolor1_string = mag_field.format(self.xcolor1)
+            xcolor2_string = mag_field.format(self.xcolor2)
+            ycolor1_string = mag_field.format(self.ycolor1)
+            ycolor2_string = mag_field.format(self.ycolor2)
+            if not catalog_instance.has_quantities([xcolor1_string,xcolor2_string,ycolor1_string,ycolor2_string]):
+                continue
+            #return TestResult(skipped=True, summary='Missing magnitudes in catalog')
+            data = catalog_instance.get_quantities([xcolor1_string,xcolor2_string,ycolor1_string,ycolor2_string])
+            xcolor = data[xcolor1_string] - data[xcolor2_string]
+            ycolor = data[ycolor1_string] - data[ycolor2_string]
 
-        data = catalog_instance.get_quantities([self.xcolor1,self.xcolor2,self.ycolor1,self.ycolor2])
-        xcolor = data[self.xcolor1] - data[self.xcolor2]
-        ycolor = data[self.ycolor1] - data[self.ycolor2]
+            fig, ax = plt.subplots()
 
-        fig, ax = plt.subplots()
+            for ax_this in (ax, self.summary_ax):
+                ax_this.set_xlabel('{} - {}'.format(xcolor1_string,xcolor2_string),fontsize=14,labelpad=2)		
+                ax_this.set_ylabel('{} - {}'.format(ycolor1_string,ycolor2_string),fontsize=14,labelpad=2)
+                ax_this.hexbin(xcolor,ycolor,gridsize=(100),cmap='GnBu',mincnt=1,bins='log')
 
-        for ax_this in (ax, self.summary_ax):
-            #ax_this.plot(data, label=catalog_name)
-            ax_this.set_xlabel('{} - {}'.format(self.xcolor1,self.xcolor2),fontsize=14,labelpad=2)		
-            ax_this.set_ylabel('{} - {}'.format(self.ycolor1,self.ycolor2),fontsize=14,labelpad=2)
-            ax_this.hexbin(xcolor,ycolor,gridsize=(100),cmap='GnBu',mincnt=1,bins='log')
-
-        self.post_process_plot(ax)
-        fig.savefig(os.path.join(output_dir, 'colors.png'))
-        plt.close(fig)
+                self.post_process_plot(ax, catalog_name)
+                fig.savefig(os.path.join(output_dir, '{}{}_{}{}_{}_{}.png'.format(self.xcolor1,self.xcolor2,self.ycolor1,self.ycolor2,mag_field,catalog_name)))
+                plt.close(fig)
 
         #score = data[0] #calculate your summary statistics
         return TestResult(inspect_only=True)
 
 
     def conclude_test(self, output_dir):
-        self.post_process_plot(self.summary_ax)
-        self.summary_fig.savefig(os.path.join(output_dir, 'colors.png'))
+        #self.post_process_plot(self.summary_ax, catalog_name)
+        #self.summary_fig.savefig(os.path.join(output_dir, 'colors.png'))
         plt.close(self.summary_fig)
