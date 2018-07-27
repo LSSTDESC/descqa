58e6c15b0f6e20f49a13734ce9f56b5ea95e9393
Joe DeRose <joe.derose13@berkeley.edu>
2018-07-27 12:11:43 -0700

    Updated red sequence test to plot full central color distributions as well as means and scatters in mass bins. No longer needs redmapper catalogs

--- descqa/RedSequence.py
+++ descqa/RedSequence.py
@@ -21,28 +21,36 @@ class RedSequence(BaseValidationTest):
         self.kwargs = kwargs
         self.bands = ['g', 'r', 'i', 'z']
         self.n_bands = len(self.bands)
-        
+
         self.validation_catalog = kwargs.get('validation_catalog', 'des_y1')
         self.rs_z = fits.open(rs_path.format(self.validation_catalog, 'z'))[0].data[:-1]
         self.rs_mean = fits.open(rs_path.format(self.validation_catalog, 'c'))[0].data[:-1]
         self.rs_sigma = fits.open(rs_path.format(self.validation_catalog, 's'))[0].data[...,:-1]
-        
+
         self.z_bins = np.linspace(*kwargs.get('z_bins', (0.0, 1.0, 31)))
 
         self.c_bins = np.linspace(*kwargs.get('c_bins', (-0.5, 2.0, 101)))
         
-        self.dz     = self.z_bins[1:] - self.z_bins[:-1]
-        self.dc     = self.c_bins[1:] - self.c_bins[:-1]
+        self.mass_bins = np.logspace(*kwargs.get('mass_bins', (12.5, 14, 4)))
+
+        self.dz = self.z_bins[1:] - self.z_bins[:-1]
+        self.dz = np.hstack([self.dz, np.array([self.dz[-1]])])
+        self.dc = self.c_bins[1:] - self.c_bins[:-1]
+        self.dm = self.mass_bins[1:] - self.mass_bins[:-1]
 
         self.n_z_bins = len(self.z_bins)-1
         self.n_c_bins = len(self.c_bins)-1
-        
+        self.n_mass_bins = len(self.mass_bins) - 1
+    
         self.z_mean = (self.z_bins[1:] + self.z_bins[:-1]) / 2
         self.c_mean = (self.c_bins[1:] + self.c_bins[:-1]) / 2
+        self.mass_mean = (self.mass_bins[1:] + self.mass_bins[:-1]) / 2
+        
+        self.use_redmapper = kwargs.get('use_redmapper', False)
 
         possible_mag_fields = ('mag_true_{}_lsst',
                                'mag_true_{}_des',
-                               'mag_true_{}_sdss',
+                              'mag_true_{}_sdss',
                               )
         self.possible_mag_fields = [[f.format(self.bands[i]) for f in possible_mag_fields]
                                     for i in range(len(self.bands))]
@@ -54,8 +62,8 @@ class RedSequence(BaseValidationTest):
                                        'Mag_true_r_sdss_z0',
                                        'Mag_true_r_sdss_z01',
                                    )
-        
-        
+                               
+                               
     def prepare_galaxy_catalog(self, gc):
 
         quantities_needed = {'redshift_true', 'is_central', 'halo_mass', 'halo_id', 'galaxy_id'}
@@ -86,22 +94,24 @@ class RedSequence(BaseValidationTest):
         if prepared is None:
             return TestResult(skipped=True)
 
-        try:
-            redmapper = GCRCatalogs.load_catalog(catalog_name+'_redmapper')
-        except:
-            return TestResult(skipped=True)
-        print(redmapper)
-        redmapper = redmapper.get_quantities(['galaxy_id'])
+        if self.use_redmapper:
+            try:
+                redmapper = GCRCatalogs.load_catalog(catalog_name+'_redmapper')
+            except:
+                return TestResult(skipped=True)
+            
+            redmapper = redmapper.get_quantities(['galaxy_id'])
+            
         absolute_magnitude_field, mag_fields, quantities_needed = prepared
-        bins     = (self.z_bins, self.c_bins)
-        hist_cen = np.zeros((self.n_z_bins, self.n_c_bins, self.n_bands-1))
+        bins     = (self.z_bins, self.c_bins, self.mass_bins)
+        hist_cen = np.zeros((self.n_z_bins, self.n_c_bins, self.n_mass_bins, self.n_bands-1))
         hist_sat = np.zeros_like(hist_cen)
         hist_mem_cen = np.zeros_like(hist_cen)
         hist_mem_sat = np.zeros_like(hist_cen)
         
-
-        cen_query = GCRQuery('is_central & (halo_mass > 5e13) & ({} < -19)'.format(absolute_magnitude_field))
-        sat_query = GCRQuery('(~is_central) & (halo_mass > 5e13) & ({} < -19)'.format(absolute_magnitude_field))
+        print(absolute_magnitude_field)
+        cen_query = GCRQuery('is_central & ({} < -19)'.format(absolute_magnitude_field))
+        sat_query = GCRQuery('(~is_central) & ({} < -19)'.format(absolute_magnitude_field))
         
         if 'r_host' in quantities_needed and 'r_vir' in quantities_needed:
             sat_query &= GCRQuery('r_host < r_vir')
@@ -110,43 +120,30 @@ class RedSequence(BaseValidationTest):
         for data in catalog_instance.get_quantities(quantities_needed, return_iterator=True):
             cen_mask = cen_query.mask(data)
             sat_mask = sat_query.mask(data)
-            mem_mask = np.in1d(data['galaxy_id'], redmapper['galaxy_id'])
-            print(catalog_instance)
-            print(data['galaxy_id'])
-            print(redmapper['galaxy_id'])
-            print(mem_mask.any())
+            if self.use_redmapper:
+                mem_mask = np.in1d(data['galaxy_id'], redmapper['galaxy_id'])
 
             for i in range(self.n_bands-1):
                 color = data[mag_fields[i]] - data[mag_fields[i+1]]
 
-                hdata = np.stack([data['redshift_true'], color]).T            
-                hist_cen[:,:,i] += np.histogramdd(hdata[cen_mask], bins)[0]
-                hist_sat[:,:,i] += np.histogramdd(hdata[sat_mask], bins)[0]
-                hist_mem_cen[:,:,i] += np.histogramdd(hdata[mem_mask & cen_mask], bins)[0]
-                hist_mem_sat[:,:,i] += np.histogramdd(hdata[mem_mask & sat_mask], bins)[0]
+                hdata = np.stack([data['redshift_true'], color, data['halo_mass']]).T            
+                hist_cen[:,:,:,i] += np.histogramdd(hdata[cen_mask], bins)[0]
+                hist_sat[:,:,:,i] += np.histogramdd(hdata[sat_mask], bins)[0]
+                if self.use_redmapper:
+                    hist_mem_cen[:,:,:,i] += np.histogramdd(hdata[mem_mask & cen_mask], bins)[0]
+                    hist_mem_sat[:,:,:,i] += np.histogramdd(hdata[mem_mask & sat_mask], bins)[0]
 
         data = cen_mask = sat_mask = mem_mask = None
         
         rs_mean, rs_scat, red_frac_sat, red_frac_cen = self.compute_summary_statistics(hist_sat, hist_cen,
                                                                                     hist_mem_sat, hist_mem_cen)
 
-        fn = os.path.join(output_dir, 'summary.pkl')
-
         red_seq = {'rs_mean':rs_mean,
                    'rs_scat':rs_scat,
                    'red_frac_sat':red_frac_sat,
-                   'red_frac_cen':red_frac_cen,
-                   'hist_cen':hist_cen,
-                   'hist_sat':hist_sat,
-                   'hist_mem_cen':hist_mem_cen,
-                   'hist_mem_sat':hist_mem_sat
-        }
-
-        with open(fn, 'wb') as fp:
-            pickle.dump(red_seq, fp)
+                   'red_frac_cen':red_frac_cen}
         
-
-        self.make_plot(red_seq, catalog_name, os.path.join(output_dir, 'red_sequence.png'))
+        self.make_plot(red_seq, hist_cen, hist_sat, hist_mem_cen, hist_mem_sat, catalog_name, os.path.join(output_dir, 'red_sequence.png'))
 
         return TestResult(inspect_only=True)
     
@@ -155,27 +152,43 @@ class RedSequence(BaseValidationTest):
         Calculate mean, and scatter of red sequence and red fraction. 
         """
 
-        tot_sat = np.sum(hist_sat, axis=(1,2))
-        tot_cen = np.sum(hist_cen, axis=(1,2))
-        tot_sat_mem = np.sum(hist_mem_sat, axis=(1,2))
-        tot_cen_mem = np.sum(hist_mem_cen, axis=(1,2))
+        tot_sat = np.sum(hist_sat, axis=(1,3))
+        tot_cen = np.sum(hist_cen, axis=(1,3))
+        tot_sat_mem = np.sum(hist_mem_sat, axis=(1,3))
+        tot_cen_mem = np.sum(hist_mem_cen, axis=(1,3))
         
-        rs_mean = np.sum(self.c_mean.reshape(1,-1,1) * (hist_mem_sat + hist_mem_cen) * self.dc.reshape(1,-1,1), axis=1) / np.sum((hist_mem_sat + hist_mem_cen) * self.dc.reshape(1,-1,1), axis=1)
-        rs_scat = np.sqrt(np.sum((self.c_mean.reshape(1,-1,1) - rs_mean.reshape(-1,1,self.n_bands-1)) ** 2 * (hist_mem_sat + hist_mem_cen) * self.dc.reshape(1,-1,1), axis=1) / np.sum((hist_mem_sat + hist_mem_cen) * self.dc.reshape(1,-1,1), axis=1))
         
-        red_frac_sat = 1 - np.sum((hist_sat - hist_mem_sat)/tot_sat.reshape(-1,1,1), axis=(1,2))
-        red_frac_cen = 1 - np.sum((hist_cen - hist_mem_cen)/tot_cen.reshape(-1,1,1), axis=(1,2))
+        if self.use_redmapper:
+            rs_mean = np.sum(self.c_mean.reshape(1,-1,1,1) * (hist_mem_sat + hist_mem_cen) * self.dc.reshape(1,-1,1,1), axis=1) / np.sum((hist_mem_sat + hist_mem_cen) * self.dc.reshape(1,-1,1,1), axis=1)
+            rs_scat = np.sqrt(np.sum((self.c_mean.reshape(1,-1,1,1) - rs_mean.reshape(-1,1,self.n_mass_bins,self.n_bands-1)) ** 2 * (hist_mem_sat + hist_mem_cen) * self.dc.reshape(1,-1,1,1), axis=1) / np.sum((hist_mem_sat + hist_mem_cen) * self.dc.reshape(1,-1,1,1), axis=1))
+            
+            red_frac_sat = 1 - np.sum((hist_sat - hist_mem_sat)/tot_sat.reshape(-1,1,1,1), axis=(1,2))
+            red_frac_cen = 1 - np.sum((hist_cen - hist_mem_cen)/tot_cen.reshape(-1,1,1,1), axis=(1,2))
+        else:
+            rs_mean = np.sum(self.c_mean.reshape(1,-1,1,1) * (hist_sat + hist_cen) * self.dc.reshape(1,-1,1,1), axis=1) / np.sum((hist_sat + hist_cen) * self.dc.reshape(1,-1,1,1), axis=1)
+            rs_scat = np.sqrt(np.sum((self.c_mean.reshape(1,-1,1,1) - rs_mean.reshape(-1,1,self.n_mass_bins,self.n_bands-1)) ** 2 * (hist_sat + hist_cen) * self.dc.reshape(1,-1,1,1), axis=1) / np.sum((hist_sat + hist_cen) * self.dc.reshape(1,-1,1,1), axis=1))
+            
+            red_frac_sat = None
+            red_frac_cen = None
         
         return rs_mean, rs_scat, red_frac_sat, red_frac_cen
 
-    def make_plot(self, red_seq, name, save_to):
-        fig, ax = plt.subplots(2, self.n_bands-1, sharex=True, sharey=False, figsize=(12,10), dpi=100)
+    def make_plot(self, red_seq, hist_cen, hist_sat, hist_mem_cen, hist_mem_sat, name, save_to):
+        fig, ax = plt.subplots(2, self.n_bands-1, sharex=True, sharey=True, figsize=(12,10), dpi=100)
 
         for i in range(self.n_bands-1):
-            ax[0,i].plot(self.z_mean, red_seq['rs_mean'][:,i])
+            for j in range(self.n_mass_bins):
+                ax[0,i].plot(self.z_mean, red_seq['rs_mean'][:,j,i], label=r'{:.2E} < $M_h$ < {:.2E}'.format(self.mass_bins[j], self.mass_bins[j+1]))
+
+            if not self.use_redmapper:
+                ax[0,i].imshow(np.sum(hist_cen[:,:,:,i], axis=2).T[::-1,:], extent=[self.z_bins[0], self.z_bins[-1], self.c_bins[0], self.c_bins[-1]])
+            else:
+                ax[0,i].imshow((np.sum(hist_mem_cen + hist_mem_sat, axis=2))[:,:,i].T[::-1,:], extent=[self.z_bins[0], self.z_bins[-1], self.c_bins[0], self.c_bins[-1]])
+                
             ax[0,i].plot(self.rs_z, self.rs_mean[:,i], label=self.validation_catalog)
 
-            ax[1,i].plot(self.z_mean, red_seq['rs_scat'][:,i])
+            for j in range(self.n_mass_bins):
+                ax[1,i].plot(self.z_mean, red_seq['rs_scat'][:,j,i], label=r'{:.2E} < $M_h$ < {:.2E}'.format(self.mass_bins[j], self.mass_bins[j+1]))
             ax[1,i].plot(self.rs_z, self.rs_sigma[i,i,:], label=self.validation_catalog)
 
             ax[0,i].set_ylabel(r'$\bar{%s-%s}$'% (self.bands[i], self.bands[i+1]))
@@ -192,3 +205,22 @@ class RedSequence(BaseValidationTest):
         fig.tight_layout()
         fig.savefig(save_to)
         plt.close(fig)                
+                            
+
+        if self.use_redmapper:
+            fig, ax = plt.subplots(1, sharex=True, sharey=True, figsize=(12,10), dpi=100)
+            
+            ax.plot(self.z_mean, red_seq['red_frac_sat'], label='satellites')
+            ax.plot(self.z_mean, red_seq['red_frac_cen'], label='centrals')
+
+            ax.legend(loc='lower right', frameon=False, fontsize='medium')
+
+            ax = fig.add_subplot(111, frameon=False)
+            ax.tick_params(labelcolor='none', top='off', bottom='off', left='off', right='off')
+            ax.grid(False)
+            ax.set_ylabel(r'f_red')
+            ax.set_xlabel(r'$z$')
+            ax.set_title(name) 
+            save_to = save_to.replace('red_sequence', 'red_fraction')
+            fig.savefig(save_to)
+            plt.close(fig)                                 
