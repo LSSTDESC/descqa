33d40f663b8ef7d76152067f14dfc2e0bcdee038
fjaviersanchez <francs1@uci.edu>
2018-08-15 11:26:54 -0700

    First commit of Density test

---
+++ descqa/DensityVersusExtinction.py
@@ -0,0 +1,51 @@
+from __future__ import unicode_literals, absolute_import, division
+import os
+import numpy as np
+from scipy.stats import binned_statistic
+from .base import BaseValidationTest, TestResult
+from .plotting import plt
+import healpy as hp
+
+__all__ = ['DensityVersusExtinction']
+
+def create_hp_map(ra, dec, nside):
+    """
+    Auxiliary function to generate HEALPix maps from catalogs.
+    It reads the ra and dec in degrees and returns a HEALPix map
+    """
+    pixnums = hp.ang2pix(nside, np.pi/2-np.radians(dec), np.radians(ra))
+    return np.bincount(pixnums, minlength=hp.nside2npix(nside))
+
+class DensityVersusExtinction(BaseValidationTest):
+    """
+    This tests checks that the object density decreases with increasing
+    MW extinction
+    """
+    def __init__(self,**kwargs): # pylint: disable=W0231
+        self.kwargs = kwargs
+        self.test_name = kwargs['test_name']
+        self.validation_path = kwargs['validation_map_filename']
+        self.nside = kwargs['nside'] 
+        self.validation_data = hp.ud_grade(hp.read_map(self.validation_path), nside_out=self.nside)
+        self.xlabel = kwargs['xlabel']
+     
+    def run_on_single_catalog(self, catalog_instance, catalog_name, output_dir):
+        
+        catalog_data = catalog_instance.get_quantities(['ra', 'dec', 'extendedness'])
+        galaxies = catalog_data['extendedness']==1 
+        data_map = create_hp_map(catalog_data['ra'][galaxies], catalog_data['dec'][galaxies], self.nside)
+        mask = data_map>0 # This is a good approximation if the pixels are big enough
+        xmin = np.percentile(self.validation_data[mask], 5)
+        xmax = np.percentile(self.validation_data[mask], 95)
+        data_map = data_map/(3600*hp.nside2pixarea(self.nside, degrees=True)) # To get the density in arcmin^-2
+        mean_dens, be, _ = binned_statistic(self.validation_data[mask], data_map[mask], statistic='mean', range=(xmin, xmax))
+        std_dens, be, _  = binned_statistic(self.validation_data[mask], data_map[mask], statistic='std', range=(xmin, xmax))
+        counts, be, _ = binned_statistic(self.validation_data[mask], data_map[mask], statistic='count', range=(xmin, xmax))
+        bin_centers = 0.5*be[1:]+0.5*be[:-1]
+        fig, ax = plt.subplots(1,1)
+        ax.errorbar(bin_centers, mean_dens, std_dens/np.sqrt(counts), fmt='o')
+        ax.set_xlabel(self.xlabel)
+        ax.set_ylabel('Mean density [arcmin$^{-2}$]')     
+        fig.savefig(os.path.join(output_dir, '%s_density_vs_extinction.png' % catalog_name))
+
+        return TestResult(inspect_only=True)
