0e48250431d95ba61c9b66f4b084926192e4c7b5
fjaviersanchez <francs1@uci.edu>
2018-08-16 09:57:43 -0700

    Addressed @yymao comments

--- descqa/DensityVersusSkyPosition.py
+++ descqa/DensityVersusSkyPosition.py
@@ -6,22 +6,23 @@ from .base import BaseValidationTest, TestResult
 from .plotting import plt
 import healpy as hp
 
-__all__ = ['DensityVersusExtinction']
+__all__ = ['DensityVersusSkyPosition']
 
 def create_hp_map(ra, dec, nside):
     """
     Auxiliary function to generate HEALPix maps from catalogs.
     It reads the ra and dec in degrees and returns a HEALPix map
     """
-    pixnums = hp.ang2pix(nside, np.pi/2-np.radians(dec), np.radians(ra))
-    return np.bincount(pixnums, minlength=hp.nside2npix(nside))
+    pixnums = hp.ang2pix(nside, ra, dec, lonlat=True)
+    return 1.0*np.bincount(pixnums, minlength=hp.nside2npix(nside))
 
-class DensityVersusExtinction(BaseValidationTest):
+class DensityVersusSkyPosition(BaseValidationTest):
     """
-    This tests checks that the object density decreases with increasing
-    MW extinction
+    This tests checks the object density as a function
+    of another map (e.g: extinction, airmass, etc)
     """
     def __init__(self,**kwargs): # pylint: disable=W0231
+        
         self.kwargs = kwargs
         self.test_name = kwargs['test_name']
         self.validation_path = kwargs['validation_map_filename']
@@ -31,13 +32,14 @@ class DensityVersusExtinction(BaseValidationTest):
      
     def run_on_single_catalog(self, catalog_instance, catalog_name, output_dir):
         
-        catalog_data = catalog_instance.get_quantities(['ra', 'dec', 'extendedness'])
-        galaxies = catalog_data['extendedness']==1 
-        data_map = create_hp_map(catalog_data['ra'][galaxies], catalog_data['dec'][galaxies], self.nside)
+        if not catalog_instance.has_quantities(['ra', 'dec', 'extendedness']):
+            return TestResult(skipped=True, summary='catalog does not have needed quantities')
+
+        catalog_data = catalog_instance.get_quantities(['ra', 'dec', 'extendedness'], filters=['extendedness == 1'])
+        data_map = create_hp_map(catalog_data['ra'], catalog_data['dec'], self.nside) 
         mask = data_map>0 # This is a good approximation if the pixels are big enough
-        xmin = np.percentile(self.validation_data[mask], 5)
-        xmax = np.percentile(self.validation_data[mask], 95)
-        data_map = data_map/(3600*hp.nside2pixarea(self.nside, degrees=True)) # To get the density in arcmin^-2
+        xmin, xmax = np.percentile(self.validation_data[mask], [5,95])
+        data_map /= (3600*hp.nside2pixarea(self.nside, degrees=True)) # To get the density in arcmin^-2
         mean_dens, be, _ = binned_statistic(self.validation_data[mask], data_map[mask], statistic='mean', range=(xmin, xmax))
         std_dens, be, _  = binned_statistic(self.validation_data[mask], data_map[mask], statistic='std', range=(xmin, xmax))
         counts, be, _ = binned_statistic(self.validation_data[mask], data_map[mask], statistic='count', range=(xmin, xmax))
