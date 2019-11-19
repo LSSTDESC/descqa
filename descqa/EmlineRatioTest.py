6ae090a9638ce98bf41ee7b74f30c9288386acec
Adam Broussard <adamcbroussard@physics.rutgers.edu>
2019-11-19 13:51:39 -0800

    Trying to placate Travis

--- descqa/EmlineRatioTest.py
+++ descqa/EmlineRatioTest.py
@@ -1,13 +1,11 @@
 from __future__ import unicode_literals, absolute_import, division
 import os
 import numpy as np
-from descqa import BaseValidationTest, TestResult
-from matplotlib import pyplot as plt
-import GCRCatalogs
-from GCRCatalogs import GCRQuery
 from astropy import units as u
 from astropy.cosmology import Planck15 as cosmo
-from scipy.stats import ks_2samp
+from matplotlib import pyplot as plt
+from descqa import BaseValidationTest, TestResult
+from GCR import GCRQuery
 from pandas import read_csv
 from matplotlib.colors import LogNorm
 
@@ -50,20 +48,18 @@ class EmlineRatioTest(BaseValidationTest):
                                                 'mag_i_lsst',
                                                 'mag_z_lsst',
                                                 'mag_y_lsst',
-                                                'galaxyID', 
-                                                'emissionLines/totalLineLuminosity:oxygenII3726', 
-                                                'emissionLines/totalLineLuminosity:oxygenII3729', 
-                                                'redshift',  
-                                                'emissionLines/totalLineLuminosity:balmerAlpha6563', 
-                                                'emissionLines/totalLineLuminosity:balmerBeta4861',  
-                                                'emissionLines/totalLineLuminosity:nitrogenII6584',  
-                                                'emissionLines/totalLineLuminosity:oxygenIII4959',  
-                                                'emissionLines/totalLineLuminosity:oxygenIII5007', 
-                                                'emissionLines/totalLineLuminosity:sulfurII6716', 
+                                                'galaxyID',
+                                                'emissionLines/totalLineLuminosity:oxygenII3726',
+                                                'emissionLines/totalLineLuminosity:oxygenII3729',
+                                                'redshift',
+                                                'emissionLines/totalLineLuminosity:balmerAlpha6563',
+                                                'emissionLines/totalLineLuminosity:balmerBeta4861',
+                                                'emissionLines/totalLineLuminosity:nitrogenII6584',
+                                                'emissionLines/totalLineLuminosity:oxygenIII4959',
+                                                'emissionLines/totalLineLuminosity:oxygenIII5007',
+                                                'emissionLines/totalLineLuminosity:sulfurII6716',
                                                 'emissionLines/totalLineLuminosity:sulfurII6731']):
             return TestResult(skipped=True, summary='do not have needed quantities')
-
-        # gc = GCRCatalogs.load_catalog(catname)
 
         uband_maglim = GCRQuery((np.isfinite, 'mag_u_lsst'), 'mag_u_lsst < 26.3')
         gband_maglim = GCRQuery((np.isfinite, 'mag_g_lsst'), 'mag_g_lsst < 27.5')
@@ -73,39 +69,39 @@ class EmlineRatioTest(BaseValidationTest):
         yband_maglim = GCRQuery((np.isfinite, 'mag_y_lsst'), 'mag_y_lsst < 24.9')
 
 
-        data = catalog_instance.get_quantities(['galaxyID', 
-                                  'emissionLines/totalLineLuminosity:oxygenII3726', 
-                                  'emissionLines/totalLineLuminosity:oxygenII3729', 
-                                  'redshift',  
-                                  'emissionLines/totalLineLuminosity:balmerAlpha6563', 
-                                  'emissionLines/totalLineLuminosity:balmerBeta4861',  
-                                  'emissionLines/totalLineLuminosity:nitrogenII6584',  
-                                  'emissionLines/totalLineLuminosity:oxygenIII4959',  
-                                  'emissionLines/totalLineLuminosity:oxygenIII5007', 
-                                  'emissionLines/totalLineLuminosity:sulfurII6716', 
-                                  'emissionLines/totalLineLuminosity:sulfurII6731',  
-                                  'mag_u_lsst', 
-                                  'mag_g_lsst',  
-                                  'mag_r_lsst',  
-                                  'mag_i_lsst',  
-                                  'mag_z_lsst',  
-                                  'mag_y_lsst'], filters = (uband_maglim | gband_maglim | rband_maglim | iband_maglim | zband_maglim | yband_maglim)) 
-        sz = data['redshift'] 
-        galaxyID = data['galaxyID'] 
-        Halpha = data['emissionLines/totalLineLuminosity:balmerAlpha6563']* 4.4659e13*u.W/u.Hz 
-        Hbeta = data['emissionLines/totalLineLuminosity:balmerBeta4861']* 4.4659e13*u.W/u.Hz 
-        NII6584 = data['emissionLines/totalLineLuminosity:nitrogenII6584']* 4.4659e13*u.W/u.Hz 
-        OIII5007 = data['emissionLines/totalLineLuminosity:oxygenIII5007']* 4.4659e13*u.W/u.Hz 
-        OIII4959 = data['emissionLines/totalLineLuminosity:oxygenIII4959']* 4.4659e13*u.W/u.Hz 
-        OII3726 = data['emissionLines/totalLineLuminosity:oxygenII3726']* 4.4659e13*u.W/u.Hz 
-        OII3729 = data['emissionLines/totalLineLuminosity:oxygenII3729']* 4.4659e13*u.W/u.Hz 
-        SII6716 = data['emissionLines/totalLineLuminosity:sulfurII6716']* 4.4659e13*u.W/u.Hz 
-        SII6731 = data['emissionLines/totalLineLuminosity:sulfurII6731']* 4.4659e13*u.W/u.Hz 
-        SIItot = SII6716 + SII6731 
-        OIIItot = OIII5007 + OIII4959 
-        OIItot = OII3726 + OII3729 
+        data = catalog_instance.get_quantities(['galaxyID',
+                                                'emissionLines/totalLineLuminosity:oxygenII3726',
+                                                'emissionLines/totalLineLuminosity:oxygenII3729',
+                                                'redshift',
+                                                'emissionLines/totalLineLuminosity:balmerAlpha6563',
+                                                'emissionLines/totalLineLuminosity:balmerBeta4861',
+                                                'emissionLines/totalLineLuminosity:nitrogenII6584',
+                                                'emissionLines/totalLineLuminosity:oxygenIII4959',
+                                                'emissionLines/totalLineLuminosity:oxygenIII5007',
+                                                'emissionLines/totalLineLuminosity:sulfurII6716',
+                                                'emissionLines/totalLineLuminosity:sulfurII6731',
+                                                'mag_u_lsst',
+                                                'mag_g_lsst',
+                                                'mag_r_lsst',
+                                                'mag_i_lsst',
+                                                'mag_z_lsst',
+                                                'mag_y_lsst'], filters=(uband_maglim | gband_maglim | rband_maglim | iband_maglim | zband_maglim | yband_maglim)) 
+        sz = data['redshift']
+        galaxyID = data['galaxyID']
+        Halpha = data['emissionLines/totalLineLuminosity:balmerAlpha6563']* 4.4659e13*u.W/u.Hz
+        Hbeta = data['emissionLines/totalLineLuminosity:balmerBeta4861']* 4.4659e13*u.W/u.Hz
+        NII6584 = data['emissionLines/totalLineLuminosity:nitrogenII6584']* 4.4659e13*u.W/u.Hz
+        OIII5007 = data['emissionLines/totalLineLuminosity:oxygenIII5007']* 4.4659e13*u.W/u.Hz
+        OIII4959 = data['emissionLines/totalLineLuminosity:oxygenIII4959']* 4.4659e13*u.W/u.Hz
+        OII3726 = data['emissionLines/totalLineLuminosity:oxygenII3726']* 4.4659e13*u.W/u.Hz
+        OII3729 = data['emissionLines/totalLineLuminosity:oxygenII3729']* 4.4659e13*u.W/u.Hz
+        SII6716 = data['emissionLines/totalLineLuminosity:sulfurII6716']* 4.4659e13*u.W/u.Hz
+        SII6731 = data['emissionLines/totalLineLuminosity:sulfurII6731']* 4.4659e13*u.W/u.Hz
+        SIItot = SII6716 + SII6731
+        OIIItot = OIII5007 + OIII4959
+        OIItot = OII3726 + OII3729
 
-        indices = np.random.choice(np.arange(len(Halpha)), size = self.sim_drawnum, replace = False)
+        indices = np.random.choice(np.arange(len(Halpha)), size=self.sim_drawnum, replace=False)
 
         sz_small = sz[indices]
         galaxyID_small = galaxyID[indices]
@@ -119,7 +115,7 @@ class EmlineRatioTest(BaseValidationTest):
 
             property_list[x] = (property_list[x][indices]/(4*np.pi*lumdist_small**2)).to('erg/s/cm**2/Hz').value
 
-        Halpha_small, Hbeta_small, NII6584_small, OIII5007_small, OIII4959_small, OII3726_small, OII3729_small, SII6716_small, SII6731_small, SIItot_small, OIIItot_small, OIItot_small = property_list        
+        Halpha_small, Hbeta_small, NII6584_small, OIII5007_small, OIII4959_small, OII3726_small, OII3729_small, SII6716_small, SII6731_small, SIItot_small, OIIItot_small, OIItot_small = property_list
 
         self.id = galaxyID_small
         self.ha = Halpha_small
@@ -146,13 +142,13 @@ class EmlineRatioTest(BaseValidationTest):
 
 
         if np.log10(pvalue) >= -4. and np.linalg.norm(medianshift) <= 0.25:
-            return TestResult(pvalue, passed = True)
+            return TestResult(pvalue, passed=True)
         elif np.linalg.norm(medianshift) <= 0.25:
-            return TestResult(pvalue, passed = False, summary='P-value must exceed 1e-4.')
+            return TestResult(pvalue, passed=False, summary='P-value must exceed 1e-4.')
         elif np.log10(pvalue) >= -4.:
-            return TestResult(pvalue, passed = False, summary = 'Total median shift must be less than or equal to 0.25 dex.')
+            return TestResult(pvalue, passed=False, summary='Total median shift must be less than or equal to 0.25 dex.')
         else:
-            return TestResult(pvalue, passed = False, summary = 'P-value must exceed 1e-4 and total median shift must be less than or equal to 0.25 dex.')
+            return TestResult(pvalue, passed=False, summary='P-value must exceed 1e-4 and total median shift must be less than or equal to 0.25 dex.')
 
 
     def makeplot(self, catalog_name):
@@ -165,10 +161,10 @@ class EmlineRatioTest(BaseValidationTest):
         sp1 = fig.add_subplot(121)
         sp2 = fig.add_subplot(122)
 
-        dist1 = [[],[]]
-        dist2 = [[],[]]
+        dist1 = [[], []]
+        dist2 = [[], []]
 
-        for subplot, cat, dist in [[sp2, self.sdsscat, dist1],[sp1, self, dist2]]:
+        for subplot, cat, dist in [[sp2, self.sdsscat, dist1], [sp1, self, dist2]]:
 
             emline1 = getattr(cat, self.emline_ratio1.split('/')[0])
             emline2 = getattr(cat, self.emline_ratio1.split('/')[1])
@@ -188,22 +184,22 @@ class EmlineRatioTest(BaseValidationTest):
         dist1 = np.array(dist1)
         dist2 = np.array(dist2)
 
-        sp1.hist2d(*dist1, bins = 50, range = [[-1.2, 1.2],[-1.25, 1]], norm = LogNorm(), cmap = 'plasma_r')
-        sp2.hist2d(*dist2, bins = 50, range = [[-1.2, 1.2],[-1.25, 1]], norm = LogNorm(), cmap = 'plasma_r')
+        sp1.hist2d(*dist1, bins=50, range=[[-1.2, 1.2],[-1.25, 1]], norm=LogNorm(), cmap='plasma_r')
+        sp2.hist2d(*dist2, bins=50, range=[[-1.2, 1.2],[-1.25, 1]], norm=LogNorm(), cmap='plasma_r')
 
-        sdss_draw_inds = np.random.choice(np.arange(len(dist1[0])), size = self.sdss_drawnum)
+        sdss_draw_inds = np.random.choice(np.arange(len(dist1[0])), size=self.sdss_drawnum)
         dist1 = dist1[:,sdss_draw_inds]
 
-        medianshift = np.nanmedian(dist1, axis = 1).reshape(2,1) - np.nanmedian(dist2, axis = 1).reshape(2,1)
+        medianshift = np.nanmedian(dist1, axis=1).reshape(2, 1) - np.nanmedian(dist2, axis=1).reshape(2, 1)
 
         medianmatch_dist2 = dist2 + medianshift
 
 
         pvalue, KSstat = kstest_2d(dist1, medianmatch_dist2)
 
-        sp1.set_xlabel('log(' + self.emline_ratio1 + ')', fontsize = 20)
-        sp1.set_ylabel('log(' + self.emline_ratio2 + ')', fontsize = 20)
-        sp2.set_xlabel('log(' + self.emline_ratio1 + ')', fontsize = 20)
+        sp1.set_xlabel('log(' + self.emline_ratio1 + ')', fontsize=20)
+        sp1.set_ylabel('log(' + self.emline_ratio2 + ')', fontsize=20)
+        sp2.set_xlabel('log(' + self.emline_ratio1 + ')', fontsize=20)
         sp1.set_xlim(-1.2, 1.2)
         sp1.set_ylim(-1.25, 1)
         sp2.set_xlim(-1.2, 1.2)
@@ -211,12 +207,12 @@ class EmlineRatioTest(BaseValidationTest):
 
         sp2.set_yticklabels([])
 
-        plt.subplots_adjust(wspace = 0.0)
+        plt.subplots_adjust(wspace=0.0)
 
-        sp1.text(0.02, 0.98, 'log p = %.2f\nD = %.2f\nMed Shift = [%.2f, %.2f]' % (np.log10(pvalue), KSstat, *medianshift.T[0]), fontsize = 14, transform = sp1.transAxes, ha = 'left', va = 'top')
+        sp1.text(0.02, 0.98, 'log p = %.2f\nD = %.2f\nMed Shift = [%.2f, %.2f]' % (np.log10(pvalue), KSstat, *medianshift.T[0]), fontsize=14, transform=sp1.transAxes, ha='left', va='top')
 
-        sp1.text(0.98, 0.02, 'SDSS', fontsize = 24, ha = 'right', va = 'bottom', transform = sp1.transAxes)
-        sp2.text(0.98, 0.02, catalog_name, fontsize = 24, ha = 'right', va = 'bottom', transform = sp2.transAxes)
+        sp1.text(0.98, 0.02, 'SDSS', fontsize=24, ha='right', va='bottom', transform=sp1.transAxes)
+        sp2.text(0.98, 0.02, catalog_name, fontsize=24, ha='right', va='bottom', transform=sp2.transAxes)
 
         return fig, pvalue, KSstat, medianshift
 
@@ -224,7 +220,7 @@ class EmlineRatioTest(BaseValidationTest):
 
     def conclude_test(self, output_dir):
         for thisfig, thiscat in zip(self.figlist, self.runcat_name):
-            thisfig.savefig(os.path.join(output_dir, thiscat + '_emline_ratios.png'), bbox_inches = 'tight')
+            thisfig.savefig(os.path.join(output_dir, thiscat + '_emline_ratios.png'), bbox_inches='tight')
             plt.close(thisfig)
 
 
@@ -234,25 +230,25 @@ class EmlineRatioTest(BaseValidationTest):
 def fhCounts(x,edge):
     # computes local CDF at a given point considering all possible axis orderings
     
-    templist = [np.sum((x[0,0:] >= edge[0]) & (x[1,0:] >= edge[1])), 
+    templist = [np.sum((x[0,0:] >= edge[0]) & (x[1,0:] >= edge[1])),
                 np.sum((x[0,0:] <= edge[0]) & (x[1,0:] >= edge[1])), 
-                np.sum((x[0,0:] <= edge[0]) & (x[1,0:] <= edge[1])), 
+                np.sum((x[0,0:] <= edge[0]) & (x[1,0:] <= edge[1])),
                 np.sum((x[0,0:] >= edge[0]) & (x[1,0:] <= edge[1]))]
     return templist
 
-def kstest_2d(dist1, dist2, alpha = 0.05):
+def kstest_2d(dist1, dist2, alpha=0.05):
     
     num1 = dist1.shape[1]
     num2 = dist2.shape[1]
     
     KSstat = -np.inf
     
-    for iX in (np.arange(0,num1+num2)):
+    for iX in (np.arange(0, num1+num2)):
         
         if iX < num1:
-            edge = dist1[0:,iX]
+            edge = dist1[0:, iX]
         else:
-            edge = dist2[0:,iX-num1]
+            edge = dist2[0:, iX-num1]
         
 #         vfCDF1 = np.sum(fhCounts(dist1, edge)) / num1
 #         vfCDF2 = np.sum(fhCounts(dist2, edge)) / num2
@@ -270,9 +266,9 @@ def kstest_2d(dist1, dist2, alpha = 0.05):
     # Peacock Z calculation and P estimation
 
     n =  num1 * num2 /(num1 + num2)
-    Zn = np.sqrt(n) * KSstat;
-    Zinf = Zn / (1 - 0.53 * n**(-0.9));
-    pValue = 2 *np.exp(-2 * (Zinf - 0.5)**2);
+    Zn = np.sqrt(n) * KSstat
+    Zinf = Zn / (1 - 0.53 * n**(-0.9))
+    pValue = 2 *np.exp(-2 * (Zinf - 0.5)**2)
 
     # Clip invalid values for P
     if pValue > 1.0:
@@ -290,14 +286,14 @@ def kstest_2d(dist1, dist2, alpha = 0.05):
 
 class sdssdist:
 
-    def __init__(self, component_file = 'data/oii-oiii_hb-oiii_components.dat'):
+    def __init__(self, component_file='data/oii-oiii_hb-oiii_components.dat'):
 
-        self.weights, self.mu_x, self.mu_y, self.sigma_x, self.sigma_y, self.cov = np.loadtxt('components.dat', unpack = True)
+        self.weights, self.mu_x, self.mu_y, self.sigma_x, self.sigma_y, self.cov = np.loadtxt('components.dat', unpack=True)
             
 
 
     def draw_components(self, size):
-        component_choices, counts = np.unique(np.random.choice(np.arange(len(self.weights)), p = self.weights, size = size), return_counts = True)
+        component_choices, counts = np.unique(np.random.choice(np.arange(len(self.weights)), p=self.weights, size=size), return_counts=True)
         data_x = []
         data_y = []
         for thischoice, thiscount in zip(component_choices, counts):
@@ -320,14 +316,14 @@ class sdsscat:
 
         data = read_csv(infile)
 
-        usecols = ['z', 'z_err', 'oii_flux', 'oii_flux_err', 'oiii_flux', 'oiii_flux_err', 
-        'h_alpha_flux', 'h_alpha_flux_err', 'h_beta_flux', 'h_beta_flux_err', 
-        'lgm_tot_p50', 'lgm_tot_p16', 'lgm_tot_p84', 'sfr_tot_p50', 'sfr_tot_p16', 'sfr_tot_p84', 
-        'oh_p50', 'h_alpha_eqw', 'oiii_4959_eqw', 'oiii_5007_eqw', 'oii_3726_eqw', 'oii_3729_eqw', 'h_beta_eqw',
-        'h_alpha_eqw_err', 'oiii_4959_eqw_err', 'oiii_5007_eqw_err', 'oii_3726_eqw_err', 'oii_3729_eqw_err', 'h_beta_eqw_err']
-        newnames = ['z', 'z_err', 'oii_uncorr', 'oii_err_uncorr', 'oiii_uncorr', 'oiii_err_uncorr', 'ha_uncorr', 'ha_err_uncorr', 'hb_uncorr', 'hb_err_uncorr', 
-        'logmstar', 'logmstar_lo', 'logmstar_hi', 'sfr', 'sfr_lo', 'sfr_hi', 'o_abundance', 'ha_ew_uncorr', 'oiii4959_ew_uncorr', 'oiii5007_ew_uncorr', 'oii3726_ew_uncorr', 'oii3729_ew_uncorr', 'hb_ew_uncorr',
-        'ha_ew_err','oiii4959_ew_err_uncorr', 'oiii5007_ew_err_uncorr', 'oii3726_ew_err_uncorr', 'oii3729_ew_err_uncorr', 'hb_ew_err_uncorr']
+        usecols = ['z', 'z_err', 'oii_flux', 'oii_flux_err', 'oiii_flux', 'oiii_flux_err',
+                   'h_alpha_flux', 'h_alpha_flux_err', 'h_beta_flux', 'h_beta_flux_err',
+                   'lgm_tot_p50', 'lgm_tot_p16', 'lgm_tot_p84', 'sfr_tot_p50', 'sfr_tot_p16', 'sfr_tot_p84',
+                   'oh_p50', 'h_alpha_eqw', 'oiii_4959_eqw', 'oiii_5007_eqw', 'oii_3726_eqw', 'oii_3729_eqw', 'h_beta_eqw',
+                   'h_alpha_eqw_err', 'oiii_4959_eqw_err', 'oiii_5007_eqw_err', 'oii_3726_eqw_err', 'oii_3729_eqw_err', 'h_beta_eqw_err']
+        newnames = ['z', 'z_err', 'oii_uncorr', 'oii_err_uncorr', 'oiii_uncorr', 'oiii_err_uncorr', 'ha_uncorr', 'ha_err_uncorr', 'hb_uncorr', 'hb_err_uncorr',
+                    'logmstar', 'logmstar_lo', 'logmstar_hi', 'sfr', 'sfr_lo', 'sfr_hi', 'o_abundance', 'ha_ew_uncorr', 'oiii4959_ew_uncorr', 'oiii5007_ew_uncorr', 'oii3726_ew_uncorr', 'oii3729_ew_uncorr', 'hb_ew_uncorr',
+                    'ha_ew_err','oiii4959_ew_err_uncorr', 'oiii5007_ew_err_uncorr', 'oii3726_ew_err_uncorr', 'oii3729_ew_err_uncorr', 'hb_ew_err_uncorr']
 
         for col, name in zip(usecols, newnames):
             setattr(self, name, data[col].values)
