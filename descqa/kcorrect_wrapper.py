8ded2990533f747ccc64db98942cc8e4e58b7a0e
Yao-Yuan Mao <yymao.astro@gmail.com>
2020-12-01 20:54:41 -0500

    Bug fix / format code

--- descqa/kcorrect_wrapper.py
+++ descqa/kcorrect_wrapper.py
@@ -2,12 +2,13 @@ import numpy as np
 
 try:
     import kcorrect as kc
-except ImportError
+except ImportError:
     import warnings
     warnings.warn("Cannot import kcorrect!")
     kc = None
 
 __all__ = ["kcorrect"]
+
 
 def kcorrect(mags, magerrs, redshifts, bandshift, filters="des_filters.dat"):
     try:
@@ -15,15 +16,17 @@ def kcorrect(mags, magerrs, redshifts, bandshift, filters="des_filters.dat"):
         kc.load_filters(f=filters)
     except AttributeError:
         raise RuntimeError("No kcorrect available")
-    maggies = 10.**(-0.4*mags)
-    maggies_ivars = 1./(maggies*0.4*np.log(10)*magerrs)**2
+    maggies = 10.0 ** (-0.4 * mags)
+    maggies_ivars = 1.0 / (maggies * 0.4 * np.log(10) * magerrs) ** 2
     kcorrect_arr = np.zeros_like(magerrs)
-    for i, (maggie, z, maggies_ivar) in enumerate(zip(maggies, redshifts, maggies_ivars)):
+    for i, (maggie, z, maggies_ivar) in enumerate(
+        zip(maggies, redshifts, maggies_ivars)
+    ):
         coffs = kc.fit_nonneg(z, maggie, maggies_ivar)
-        #Reconstruct the magnitudes as observed and in the rest frame
+        # Reconstruct the magnitudes as observed and in the rest frame
         rmaggies = kc.reconstruct_maggies(coffs, redshift=z)
         reconstruct_maggies = kc.reconstruct_maggies(coffs, redshift=bandshift)
-        reconstruct_maggies = reconstruct_maggies/(1.+bandshift)
-        kcorrect_res_this = 2.5*np.log10(reconstruct_maggies/rmaggies)
+        reconstruct_maggies /= 1.0 + bandshift
+        kcorrect_res_this = 2.5 * np.log10(reconstruct_maggies / rmaggies)
         kcorrect_arr[i] = kcorrect_res_this[1:]
     return kcorrect_arr
