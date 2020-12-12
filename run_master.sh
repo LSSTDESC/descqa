1628517192407b00f0f1890c7a68a6199a8ece1a
Yao-Yuan Mao <yymao.astro@gmail.com>
2020-12-12 06:23:12 -0800

    add PYTHON

--- run_master.sh
+++ run_master.sh
@@ -8,6 +8,7 @@ set -e
 
 # activate DESC python environment
 source /global/common/software/lsst/common/miniconda/setup_current_python.sh ""
+PYTHON='python'
 
 # set output directory
 OUTPUTDIR="/global/cfs/cdirs/lsst/groups/CS/descqa/run/v2"
