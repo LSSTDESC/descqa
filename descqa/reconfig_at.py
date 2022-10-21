47f899b1ff13c0cc58a088f1f6d26b7fa6bf21a9
psferguson <petersferguson@gmail.com>
2022-10-21 13:02:45 -0700

    analysis_tools task reconfigure base class added

--- descqa/reconfig_at.py
+++ descqa/reconfig_at.py
@@ -3,6 +3,8 @@ configuration routines for analysis tools
 """
 from __future__ import unicode_literals, division, print_function, absolute_import
 import numpy as np
+
+from lsst.analysis.tools.tasks.base import _StandinPlotInfo
 from lsst.analysis.tools.analysisPlots.analysisPlots import ( 
     WPerpPSFPlot, 
     ShapeSizeFractionalDiffScatterPlot,
@@ -14,34 +16,57 @@ from lsst.analysis.tools.analysisMetrics import (
     E1DiffMetric,
     E2DiffMetric,
 )
-
-__all__ = []
-
-
-def reconfigured_shapesize(band):
-    ''' Call analysis tools wperpPSFPlot and reconfigure for DP0.2'''
-    # call base class
-    shapesize = ShapeSizeFractionalMetric()
-    shapesize_plot = ShapeSizeFractionalDiffScatterPlot()
-    shapesize_plot.produce.addSummaryPlot = False
-    # config 
-    shapesize_plot.prep.selectors.snSelector.bands = band
-
-    #shapesize_plot.prep.bands=['r']
- 
-    # populate prep
-    shapesize.populatePrepFromProcess()
-    shapesize_plot.populatePrepFromProcess()
+from .plotting import plt
 
 
-    # get list of quantities
-    key_list_full = list(shapesize.prep.getInputSchema())
-    key_list = [key_list_full[i][0] for i in range(len(key_list_full))]
-    key_list_full2 = list(shapesize_plot.prep.getInputSchema())
-    key_list2 = [key_list_full2[i][0] for i in range(len(key_list_full2))]
-    key_list.extend(key_list2)
+__all__ = ["shapeSizeFractional"]
 
-    return shapesize, shapesize_plot, key_list
+class shapeSizeFractional():
+    return_types=["plot","metric"] # add metric names? 
+    band="r"
+    metric={}
+    plot={}
+    key_list=[]
+    def reconfigure(self,band="r"):
+        ''' Call analysis tools shapesize and reconfigure for DP0.2'''
+        self.band=band
+        # call base class
+        shapesize = ShapeSizeFractionalMetric()
+        shapesize_plot = ShapeSizeFractionalDiffScatterPlot()
+        shapesize_plot.produce.addSummaryPlot = False
+        # config 
+        shapesize_plot.prep.selectors.snSelector.bands = self.band
+
+        #shapesize_plot.prep.bands=['r']
+    
+        # populate prep
+        shapesize.populatePrepFromProcess()
+        shapesize_plot.populatePrepFromProcess()
+
+        # get list of quantities
+        key_list_full = list(shapesize.prep.getInputSchema())
+        key_list = [key_list_full[i][0] for i in range(len(key_list_full))]
+        key_list_full2 = list(shapesize_plot.prep.getInputSchema())
+        key_list2 = [key_list_full2[i][0] for i in range(len(key_list_full2))]
+        key_list.extend(key_list2)
+
+        self.metric=shapesize
+        self.plot=shapesize_plot
+        self.key_list=key_list
+
+    def run(self,data,output_dir,metric=True, plot=True):
+        if metric:
+            self.metric_values = self.metric(data,band=self.band)
+            for key in self.metric_values.keys():
+                print(self.metric_values[key])
+        if plot: 
+            stage1 = self.plot.prep(data,band=self.band)
+            stage2 = self.plot.process(data,band=self.band)
+            plot = self.plot.produce(stage2, plotInfo=_StandinPlotInfo(), band=self.band,skymap='DC2')
+            plt.savefig(output_dir+"shapesizeTest.png")
+            plt.close()
+      
+
 
 def reconfigured_E1Diff(band):
     ''' Call analysis tools wperpPSFPlot and reconfigure for DP0.2'''
